a638039faacc0a2237af6557d38d5709c7a0fcfa
divya-36736 <23bec036@nith.ac.in>
2025-06-03 00:00:17 +0530

    Time: 0 ms (100%), Space: 8.8 MB (80.05%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -1,16 +1,37 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        string res;
-        int count = 0;
-        for(char c: s){
-            if(c == '(' && count++>0){
-                res += c;
-            }
-            if(c == ')' && count-->1){
-                res += c;
-            }
+        // string res;
+        // int count = 0;
+        // for(char c: s){
+        //     if(c == '(' && count++>0){
+        //         res += c;
+        //     }
+        //     if(c == ')' && count-->1){
+        //         res += c;
+        //     }
+        // }
+        // return res;
+
+
+    int openCount = 0;
+    int closeCount = 0;
+    string result = "";
+    int start = 0;
+
+    for (int i = 0; i < s.length(); i++) {
+        char c = s[i];
+        if (c == '(') {
+            openCount++;
+        } else if (c == ')') {
+            closeCount++;
         }
-        return res;
+
+        if (openCount == closeCount) {
+            result += s.substr(start+1, i-start-1);
+            start = i+1;
+        }
+    }
+    return result;
     }
 };
