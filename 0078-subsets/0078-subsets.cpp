cd57df42dd95365ab9723241d7e1dbda59e01242
divya-36736 <23bec036@nith.ac.in>
2025-06-19 12:58:33 +0530

    Time: 0 ms (100%), Space: 9.8 MB (81.13%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,29 +1,45 @@
 class Solution {
 public:
 
-    void getAllsubsets(vector<int> &nums, vector<int> &ans, int i, vector<vector<int>> &allsubsets){
-        if(i == nums.size()){
-            allsubsets.push_back({ans});
-            return;
-        }
+    // void getAllsubsets(vector<int> &nums, vector<int> &ans, int i, vector<vector<int>> &allsubsets){
+    //     if(i == nums.size()){
+    //         allsubsets.push_back({ans});
+    //         return;
+    //     }
 
-        //include
-        ans.push_back(nums[i]);
-        getAllsubsets(nums, ans, i+1, allsubsets);
+    //     //include
+    //     ans.push_back(nums[i]);
+    //     getAllsubsets(nums, ans, i+1, allsubsets);
 
-        //backtracking
-        ans.pop_back();
-        getAllsubsets(nums, ans, i+1, allsubsets);
+    //     //backtracking
+    //     ans.pop_back();
+    //     getAllsubsets(nums, ans, i+1, allsubsets);
 
-    }
+    // }
 
 
 
 
     vector<vector<int>> subsets(vector<int>& nums) {
-      vector<vector<int>> allsubsets;
-      vector<int> ans;
-      getAllsubsets(nums, ans, 0, allsubsets);
-      return allsubsets;
+    //   vector<vector<int>> allsubsets;
+    //   vector<int> ans;
+    //   getAllsubsets(nums, ans, 0, allsubsets);
+    //   return allsubsets;
+
+    int n = nums.size();
+    int subset = 1<<n;
+    vector<vector<int>>result;
+    for(int mask =0; mask<subset; ++mask){
+        vector<int> res;
+            for(int i = 0; i < n; ++i){
+                if(mask & (1 << i)){ 
+                    res.push_back(nums[i]);
+                }
+            }
+            result.push_back(res);
+        }
+
+        return result;
+    
     }
 };
