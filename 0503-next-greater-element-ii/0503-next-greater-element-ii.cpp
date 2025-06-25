2679a789e74215204c532de643e6da7240a6e4a8
divya-36736 <23bec036@nith.ac.in>
2025-06-25 14:30:02 +0530

    Time: 2 ms (79.96%), Space: 28.3 MB (91.74%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,24 +1,35 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        int n = nums.size();
-        stack<int>st;
-        vector<int>nge(n);
-        if(n == 1) nge[0] = -1;
-        for(int i = 0; i <= n-1; i++){
-            for(int j = i+1; j <= i+n-1; j++){
-                int idx = j % n;
-                if(nums[idx] > nums[i]){
-                    nge[i] = nums[idx];
-                    break;
-                }
-                else{
-                    nge[i] = -1;
-                }
+        // int n = nums.size();
+        // stack<int>st;
+        // vector<int>nge(n);
+        // if(n == 1) nge[0] = -1;
+        // for(int i = 0; i <= n-1; i++){
+        //     for(int j = i+1; j <= i+n-1; j++){
+        //         int idx = j % n;
+        //         if(nums[idx] > nums[i]){
+        //             nge[i] = nums[idx];
+        //             break;
+        //         }
+        //         else{
+        //             nge[i] = -1;
+        //         }
 
-            }
-        }
-        return nge;
+        //     }
+        // }
+        // return nge;
         
+        int n = nums.size();
+        stack<int>s;
+        vector<int>ans(n,-1);
+        for(int i= 2*n-1; i>=0; i--){
+            while(!s.empty() && nums[s.top()] <= nums[i%n]){
+                s.pop();
+            }
+            ans[i%n] = s.empty() ? -1:nums[s.top()];
+            s.push(i%n);
+        }
+        return ans;
     }
 };
