3b2f4c80f5f3d33948fced0b56f1226ee6f44474
divya-36736 <23bec036@nith.ac.in>
2025-06-27 23:15:47 +0530

    Time: 19 ms (30.77%), Space: 14.3 MB (22.47%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,21 +1,40 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        // int n = s.size();
+        // int maxi = INT_MIN;
+        // if(n == 0) return 0;
+        // for(int i = 0; i<n; i++){
+        //     unordered_set<int>set;
+        //     for(int j = i; j<n; j++){
+        //         if (set.find(s[j]) != set.end())
+        //         {
+        //             maxi = max(maxi, j - i);
+        //             break;
+        //         }
+        //     set.insert(s[j]);
+        //     maxi = max(maxi, j - i + 1);
+        //     }
+        // }
+        // return maxi;
+
+        //optimal 1;
         int n = s.size();
-        int maxi = INT_MIN;
-        if(n == 0) return 0;
-        for(int i = 0; i<n; i++){
-            unordered_set<int>set;
-            for(int j = i; j<n; j++){
-                if (set.find(s[j]) != set.end())
-                {
-                    maxi = max(maxi, j - i);
-                    break;
-                }
-            set.insert(s[j]);
-            maxi = max(maxi, j - i + 1);
+        int maxi = 0; // Initialize to 0
+        unordered_set<char> set; // Use char
+        int l = 0;
+        
+        if (n == 0) return 0; // Handle empty string
+        
+        for (int r = 0; r < n; r++) {
+            while (set.find(s[r]) != set.end()) { // Fixed condition
+                set.erase(s[l]);
+                l++;
             }
+            set.insert(s[r]);
+            maxi = max(maxi, r - l + 1);
         }
+        
         return maxi;
     }
 };
