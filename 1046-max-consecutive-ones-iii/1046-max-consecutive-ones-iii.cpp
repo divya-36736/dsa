59e656c71537caf011dad7ef0ab18ec8808d519d
divya-36736 <23bec036@nith.ac.in>
2025-06-28 16:32:52 +0530

    Time: 0 ms (100%), Space: 59.4 MB (25.65%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
+
+        //brute force approach
+
         // int maxlen = 0;
         // int n = nums.size();
         // for(int i = 0; i<n; i++){
@@ -19,23 +22,47 @@ public:
         // }
         // return maxlen;
 
-        int maxlen = 0; 
-        int l = 0; 
-        int r = 0;
+        // int maxlen = 0; 
+        // int l = 0; 
+        // int r = 0;
+        // int n = nums.size();
+        // int zero = 0;
+        // while(r < n){
+        //     if(nums[r] == 0) zero++;
+        //     while(zero > k){
+        //         if(nums[l] == 0) zero--;
+        //         l++;
+        //     }
+        //     if(zero <= k){
+        //         int len = r-l+1;
+        //         maxlen = max(len, maxlen);
+        //     }
+        //     r++;
+        // }
+        // return maxlen;
+
+        
         int n = nums.size();
+        int l = 0;
+        int r = 0;
         int zero = 0;
-        while(r < n){
-            if(nums[r] == 0) zero++;
-            while(zero > k){
-                if(nums[l] == 0) zero--;
+        int maxlen = 0;
+        
+        while (r < n) {
+            if (nums[r] == 0) {
+                zero++;
+            }
+            while (zero > k) { 
+                if (nums[l] == 0) {
+                    zero--;
+                }
                 l++;
             }
-            if(zero <= k){
-                int len = r-l+1;
-                maxlen = max(len, maxlen);
-            }
-            r++;
+           
+            maxlen = max(maxlen, r - l + 1);
+            r++; 
         }
+        
         return maxlen;
     }
 };
