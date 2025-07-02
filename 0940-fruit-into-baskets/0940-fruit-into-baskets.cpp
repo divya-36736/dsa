c39df05eef3b38736f8d0c03594e87227b51d89e
divya-36736 <23bec036@nith.ac.in>
2025-07-02 16:45:55 +0530

    Time: 47 ms (33.62%), Space: 75 MB (56.7%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -20,15 +20,36 @@ public:
 
         // optimal
 
+        // int n = fruits.size();
+        // int l = 0;
+        // int r = 0;
+        // unordered_map<int, int>m;
+        // int maxlen = 0;
+        // while(r<n){
+        //     m[fruits[r]]++;
+        //     if(m.size() > 2){
+        //         while(m.size() > 2){
+        //             m[fruits[l]]--;
+        //             if(m[fruits[l]] == 0){
+        //                 m.erase(fruits[l]);
+        //             }
+        //             l++;
+        //         }
+        //     }
+        //     maxlen = max(maxlen, r-l+1);   
+        //     r++;
+        // }
+        // return maxlen;
+
         int n = fruits.size();
-        int l = 0;
+        int l =0;
         int r = 0;
         unordered_map<int, int>m;
         int maxlen = 0;
         while(r<n){
             m[fruits[r]]++;
             if(m.size() > 2){
-                while(m.size() > 2){
+                if(m.size() > 2){
                     m[fruits[l]]--;
                     if(m[fruits[l]] == 0){
                         m.erase(fruits[l]);
@@ -36,7 +57,7 @@ public:
                     l++;
                 }
             }
-            maxlen = max(maxlen, r-l+1);   
+            maxlen = max(maxlen, r-l+1);
             r++;
         }
         return maxlen;
