5204aedb1b279b5412b5adabadc09b7876f4f335
divya-36736 <23bec036@nith.ac.in>
2025-07-04 16:46:33 +0530

    Time: 4 ms (9.6%), Space: 23.1 MB (56.57%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -11,28 +11,35 @@
  */
 class Solution {
 private: 
-    int check(TreeNode* node){
-        if(node == NULL) return 0;
-        int lh =  check(node->left);
-        if(lh == -1) return -1;
-        int rh = check(node->right);
-        if(rh == -1) return -1;
-        if(abs(lh-rh)>1) return -1;
-        return max(lh, rh) + 1;
-    }
+    // int check(TreeNode* node){
+    //     if(node == NULL) return 0;
+    //     int lh =  check(node->left);
+    //     if(lh == -1) return -1;
+    //     int rh = check(node->right);
+    //     if(rh == -1) return -1;
+    //     if(abs(lh-rh)>1) return -1;
+    //     return max(lh, rh) + 1;
+    // }
 
-// private:
-//     bool check(TreeNode* node){
-//         if(node == NULL) return true;
-//         int lh = findHleft(node->left);
-//         int rh = findHright(node->right);
-//         if(abs())
-//     }
+
 
 public:
     bool isBalanced(TreeNode* root) {
-        return check(root) != -1;
-
-        
+        // return check(root) != -1;
+        if(root == NULL) return true;
+        int lh = findHeight(root->left);
+        int rh = findHeight(root->right);
+        if(abs(rh-lh)>1) return false;
+        bool left = isBalanced(root->left);
+        bool right = isBalanced(root->right);
+        if(!left || !right) return false;
+        return true;
+    }
+private:
+    int findHeight(TreeNode* node) {
+        if (node == NULL) return 0;
+        return 1 + max(findHeight(node->left), findHeight(node->right));
     }
 };
+
+
