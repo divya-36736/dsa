e9ecce76c64a839fe88b567a1f7d586140e25f2e
divya-36736 <23bec036@nith.ac.in>
2025-07-08 22:24:31 +0530

    Time: 0 ms (100%), Space: 22 MB (18.34%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -11,19 +11,29 @@
  */
 class Solution {
 public:
-    void inorder(TreeNode* root, vector<int>&ans){
-        if(root == NULL) return;
-        inorder(root->left, ans);
-        ans.push_back(root->val);
-        inorder(root->right, ans);
+    // void inorder(TreeNode* root, vector<int>&ans){
+    //     if(root == NULL) return;
+    //     inorder(root->left, ans);
+    //     ans.push_back(root->val);
+    //     inorder(root->right, ans);
+    // }
+
+    bool helper(TreeNode* root, TreeNode* min, TreeNode* max){
+        if(root == NULL) return true;
+        if(min != NULL && root->val <= min->val) return false;
+        if(max != NULL && root->val >= max->val) return false;
+        return helper(root->left, min, root) && helper(root->right, root, max);
     }
 
     bool isValidBST(TreeNode* root) {
-        vector<int>ans;
-        inorder(root, ans);
-        for(int i = 1; i<ans.size(); i++){
-            if(ans[i] <= ans[i-1]) return false;
-        }
-        return true;
+        // vector<int>ans;
+        // inorder(root, ans);
+        // for(int i = 1; i<ans.size(); i++){
+        //     if(ans[i] <= ans[i-1]) return false;
+        // }
+        // return true;
+
+        
+        return helper(root, NULL, NULL);
     }
 };
