70e6760c5aea1cd4f1779cd43b4a65d4b399d983
divya-36736 <23bec036@nith.ac.in>
2025-07-08 23:59:55 +0530

    Time: 0 ms (100%), Space: 12.5 MB (25.74%) - LeetHub

--- 0799-minimum-distance-between-bst-nodes/0799-minimum-distance-between-bst-nodes.cpp
+++ 0799-minimum-distance-between-bst-nodes/0799-minimum-distance-between-bst-nodes.cpp
@@ -11,28 +11,42 @@
  */
 class Solution {
 public:
-    void inorder(TreeNode* root, vector<int>&ans){
-        if(root == NULL) return;
-        inorder(root->left, ans);
-        ans.push_back(root->val);
-        inorder(root->right, ans);
-    }
+    // void inorder(TreeNode* root, vector<int>&ans){
+    //     if(root == NULL) return;
+    //     inorder(root->left, ans);
+    //     ans.push_back(root->val);
+    //     inorder(root->right, ans);
+    // }
+
+    TreeNode* prev = NULL;
     int minDiffInBST(TreeNode* root) {
-        vector<int>ans;
-        vector<int> ans1;
-        int min = 0;
-
-        inorder(root, ans);
-        for(int i =1; i<ans.size(); i++){
-            ans1.push_back(ans[i] - ans[i-1]);
+        if(root == NULL) return INT_MAX;
+        int ans = INT_MAX;
+        if(root->left != NULL){
+            int leftMin = minDiffInBST(root->left);
+            ans = min(ans, leftMin);
         }
+        if(prev != NULL){
+            ans = min(ans, root->val - prev->val);
+        }
+        prev = root;
+        
+        if(root->right){
+            int rightMin = minDiffInBST(root->right);
+            ans = min(ans, rightMin);
+        }
+        return ans;
+        // vector<int>ans;
+        // vector<int> ans1;
+        // int min = 0;
 
-        sort(ans1.begin(), ans1.end());
-        return ans1[0];
-
-        // for(int i = 1; i<ans.size(); i++){
-        //     ans1.push_back(ans[i]-ans[i-1]);
+        // inorder(root, ans);
+        // for(int i =1; i<ans.size(); i++){
+        //     ans1.push_back(ans[i] - ans[i-1]);
         // }
+
+        // sort(ans1.begin(), ans1.end());
+        // return ans1[0];
 
     }
 };
