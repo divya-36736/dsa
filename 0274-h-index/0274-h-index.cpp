8fba2fd6c620a41eda5bc71522bcd62521c29412
divya-36736 <23bec036@nith.ac.in>
2025-07-10 07:39:20 +0530

    Time: 0 ms (100%), Space: 12.3 MB (64.05%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -1,18 +1,38 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
+        // sort(citations.begin(), citations.end());
+        // reverse(citations.begin(), citations.end());
+        // int n = citations.size();
+        // int count = 0;
+        
+        // for(int i = 0; i<n; i++){
+        //     if(citations[i] >= i+1){
+        //         count = i+1;
+        //     }else{
+        //         break;
+        //     }
+        // }
+        // return count;
+
+
+        //optimal
         sort(citations.begin(), citations.end());
         reverse(citations.begin(), citations.end());
         int n = citations.size();
-        int count = 0;
-        
-        for(int i = 0; i<n; i++){
-            if(citations[i] >= i+1){
-                count = i+1;
-            }else{
-                break;
+        int h = 0;
+        int st = 0;
+        int end = n-1;
+        while(st<=end){
+            int mid = st + (end - st)/2;
+            if(citations[mid] >= mid+1){
+                st = mid+1;
+                h = mid+1;
+            }
+            else{
+                end = mid-1;
             }
         }
-        return count;
+        return h;
     }
 };
