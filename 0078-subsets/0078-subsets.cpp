390e8ee12b97e3cb451ffe7ee1e57ff404f4cfe1
divya-36736 <23bec036@nith.ac.in>
2025-08-01 22:12:53 +0530

    Time: 3 ms (32.47%), Space: 15.6 MB (19.73%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -2,13 +2,16 @@ class Solution {
 public:
     void subs(int i, vector<int>& nums, vector<vector<int>>&ans, vector<int>ds){
         if(i==nums.size()){
-            
             ans.push_back(ds);
             return;
-        }
+        }       
         ds.push_back(nums[i]);
+       
+        //take
         subs(i+1, nums, ans, ds);
         ds.pop_back();
+
+        //not take
         subs(i+1, nums, ans, ds);
         return;
     }
@@ -16,6 +19,7 @@ public:
         vector<vector<int>>ans;
         vector<int>ds;
         subs(0, nums, ans, ds);
+
         return ans;
     }
 };
