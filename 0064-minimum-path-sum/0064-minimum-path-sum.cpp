297d04158abd595f7857fe88a280e9c0b991b932
divya-36736 <23bec036@nith.ac.in>
2025-08-03 09:47:47 +0530

    Time: 0 ms (100%), Space: 16.4 MB (40.75%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     int sum(int i, int j, vector<vector<int>>& grid, vector<vector<int>>&dp){
+        //base acse
         if(i == 0 && j==0){
             return grid[0][0];
         }
+        //out of bound
         if(i<0 || j<0) return 1e9;
+        //already visited
         if(dp[i][j] != -1) return dp[i][j];
-
+        //if not visisted the we compute it
         int up  = sum(i-1, j, grid, dp);
         int left = sum(i, j-1, grid, dp);
-
+        
         return dp[i][j] = grid[i][j] + min(up, left);
     }
     int minPathSum(vector<vector<int>>& grid) {
