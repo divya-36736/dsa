c58e7e52bb4d3b7eaba9a71b75e805805b95e8b3
divya-36736 <23bec036@nith.ac.in>
2025-08-05 23:26:48 +0530

    Time: 0 ms (100%), Space: 22.6 MB (50.61%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,14 +1,30 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if(nums.empty()) return 0;
-       int n = nums.size();
-       int idx = 1;
-       for(int i = 1; i<n; i++){
-            if(nums[i] != nums[i-1]){
-                nums[idx++] = nums[i];
+        //take set bcz set not allow duplicat values
+        // set<int> set;
+        // int n = nums.size();
+        //iterate from 0 to n values for insert the values in set
+        // for (int i = 0; i < n; i++) {
+        //     set.insert(nums[i]);
+        // }
+        //set size
+        // int k = set.size();
+        //we now answer give in nums not in set then the value of set is now inserted in back in nums
+        // int j = 0;
+        // for (int x : set) {
+        //     nums[j++] = x;
+        // }
+        // return k;
+
+        int n = nums.size();
+        int i = 0;
+        for(int j = 1; j<n; j++){
+            if(nums[i] != nums[j]){
+                i++;
+                nums[i] = nums[j];
+            }
         }
-       }
-    return idx;
+        return i+1;
     }
 };
