2f73eb86e345e9a353492c4c4d35d06b56716a0b
divya-36736 <23bec036@nith.ac.in>
2025-08-05 23:50:17 +0530

    Time: 0 ms (100%), Space: 30.3 MB (24.92%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,23 +1,34 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
+        // int n = nums.size();
+        // k = k%n;
+        // vector<int>temp(n);
+        // //optimal approch
+        // // reverse(nums.begin(), nums.end());
+        // // reverse(nums.begin(), nums.begin()+k);
+        // // reverse(nums.begin()+k, nums.end());
+
+        // //BRUTE approach
+        // int j = 0;
+        // for(int i = n-k; i<n; i++){
+        //     temp[j] = nums[i];
+        //     j++;
+        // }
+        // for(int i = 0; i<n-k; i++){
+        //     temp[j] = nums[i];
+        //     j++;
+        // }
+        // for(int i = 0; i<n; i++){
+        //     nums[i] = temp[i];
+        // }
+       
         int n = nums.size();
-        k = k%n;
         vector<int>temp(n);
-        // reverse(nums.begin(), nums.end());
-        // reverse(nums.begin(), nums.begin()+k);
-        // reverse(nums.begin()+k, nums.end());
-        int j = 0;
-        for(int i = n-k; i<n; i++){
-            temp[j] = nums[i];
-            j++;
-        }
-        for(int i = 0; i<n-k; i++){
-            temp[j] = nums[i];
-            j++;
-        }
         for(int i = 0; i<n; i++){
-            nums[i] = temp[i];
+            temp[(i+k)%n] = nums[i];
         }
+        //copy temp to num
+        nums = temp;
     }
 };
