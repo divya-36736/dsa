aae42429a6271b91be8f380ed50582a7eb3c0c3d
divya-36736 <23bec036@nith.ac.in>
2025-08-06 00:43:13 +0530

    Time: 3 ms (17.11%), Space: 24.9 MB (5.86%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n = nums.size();
-        vector<int>temp;
+        //int n = nums.size();
+        // vector<int>temp;
         // int nonzero = 0;
         // for(int i = 0; i<n; i++){
         //     if(nums[i] != 0){
@@ -31,20 +31,32 @@ public:
 
         //optimal approach;
 
-        int j = n; 
-        for(int i = 0; i < n; i++) {
-            if(nums[i] == 0) {
-                j = i; 
-                break;
-            }
-        }
+        // int j = n; 
+        // for(int i = 0; i < n; i++) {
+        //     if(nums[i] == 0) {
+        //         j = i; 
+        //         break;
+        //     }
+        // }
         
-        for(int i = j + 1; i < n && j < n; i++) {
-            if(nums[i] != 0) {
-                swap(nums[i], nums[j]);
-                j++;
+        // for(int i = j + 1; i < n && j < n; i++) {
+        //     if(nums[i] != 0) {
+        //         swap(nums[i], nums[j]);
+        //         j++;
+        //     }
+        // }
+
+        int n = nums.size();
+        vector<int>temp;
+        for(int i = 0; i<n; i++){
+            if(nums[i] != 0){
+                temp.push_back(nums[i]);
             }
         }
+        while(temp.size() != n){
+            temp.push_back(0);
+        }
+        nums = temp;
 
     };
 };
