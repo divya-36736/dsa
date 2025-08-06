fa761c724ae16c0b5c8f740ba7ac1ad75b160ce3
divya-36736 <23bec036@nith.ac.in>
2025-08-06 10:53:24 +0530

    Time: 3 ms (29.4%), Space: 28.4 MB (7.2%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -5,18 +5,30 @@ public:
        
         //sort(nums.begin(), nums.end());
         
-        for(int i = 0; i<n; i++){
-            int count = 0;
-            for(int j = 0; j<n; j++){
-                if(nums[i] == nums[j]){
-                    count++;
-                }
-            }
-            if(count > n/2){
-                return nums[i];
-        }
-        }
+        // for(int i = 0; i<n; i++){
+        //     int count = 0;
+        //     for(int j = 0; j<n; j++){
+        //         if(nums[i] == nums[j]){
+        //             count++;
+        //         }
+        //     }
+        //     if(count > n/2){
+        //         return nums[i];
+        // }
+        // }
         
+        // return -1;
+
+        //using map
+        map<int, int>mpp; //for value or count
+        for(int i = 0; i<n; i++){
+            mpp[nums[i]]++;
+        }
+        for(auto it: mpp){
+            if(it.second > n/2){
+                return it.first;
+            }
+        }
         return -1;
     }
 };
