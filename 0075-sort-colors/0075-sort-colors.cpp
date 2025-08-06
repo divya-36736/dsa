9d5c39fa696ef8062d26e2f195d47161c264b496
divya-36736 <23bec036@nith.ac.in>
2025-08-06 10:23:43 +0530

    Time: 0 ms (100%), Space: 11.8 MB (12.05%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -11,12 +11,19 @@ public:
         //         }
         //     }
         // }
-        for(int i = 0; i<n; i++){
-            for(int j = i+1; j<n; j++){
-                if(nums[i]>nums[j]){
-                    swap(nums[i], nums[j]);
-                }
-            }
+    //count0, count1, count2 alg alg variabal banao phir put kro
+    int count0 = 0, count1= 0, count2 = 0;
+    for(int i = 0; i<n; i++){
+        if(nums[i] == 0){
+            count0++;
+        }else if(nums[i] == 1){
+            count1++;
         }
+        else count2++;
+    }
+
+    for(int i = 0; i<count0; i++) nums[i] = 0;
+    for(int i = count0; i<count0+count1; i++) nums[i] = 1;
+    for(int i = count0+count1; i<n; i++) nums[i] = 2;
     }
 };
