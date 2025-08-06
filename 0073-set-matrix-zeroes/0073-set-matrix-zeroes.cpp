cf71eb9637041fea14ca9e1b1ae46d1afb5facfe
divya-36736 <23bec036@nith.ac.in>
2025-08-07 02:09:23 +0530

    Time: 0 ms (100%), Space: 18.5 MB (78.91%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,40 +1,62 @@
 class Solution {
 public:
-    int mark = -1e7;
-    //mark row and mark col -1
-    void markRow(vector<vector<int>>& matrix, int n, int m, int i){
-        //row fix col ko -1
-        for(int j= 0; j<m; j++){
-            if (matrix[i][j] != 0) {
-                matrix[i][j] = mark;
-        }
-        }
-    }
-    void markcol(vector<vector<int>>& matrix, int n, int m, int j){
-        //col fix row ko -1
-        for(int i = 0; i<n; i++){
-            if (matrix[i][j] != 0) {
-                matrix[i][j] = mark;
-        }
-        }
-    }
+    // int mark = -1e7;
+    // //mark row and mark col -1
+    // void markRow(vector<vector<int>>& matrix, int n, int m, int i){
+    //     //row fix col ko -1
+    //     for(int j= 0; j<m; j++){
+    //         if (matrix[i][j] != 0) {
+    //             matrix[i][j] = mark;
+    //     }
+    //     }
+    // }
+    // void markcol(vector<vector<int>>& matrix, int n, int m, int j){
+    //     //col fix row ko -1
+    //     for(int i = 0; i<n; i++){
+    //         if (matrix[i][j] != 0) {
+    //             matrix[i][j] = mark;
+    //     }
+    //     }
+    // }
     void setZeroes(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        for(int i= 0; i<n; i++){
+        // for(int i= 0; i<n; i++){
+        //     for(int j = 0; j<m; j++){
+        //         if(matrix[i][j] == 0){
+        //             markRow(matrix, n, m, i);
+        //             markcol(matrix, n, m, j);
+        //         }
+        //     }
+        // }
+        // for(int i = 0; i<n; i++){
+        //     for(int j = 0; j<m; j++){
+        //         if(matrix[i][j] == mark){
+        //             matrix[i][j] = 0;
+        //     }
+        //     }
+        // }
+
+        vector<int>row(n);
+        vector<int>col(m);
+
+        for(int i = 0; i<n; i++){
             for(int j = 0; j<m; j++){
                 if(matrix[i][j] == 0){
-                    markRow(matrix, n, m, i);
-                    markcol(matrix, n, m, j);
+                    //mark ith index with 1
+                    row[i] = 1;
+                    //mark jth index with 1
+                    col[j] = 1;
                 }
             }
         }
         for(int i = 0; i<n; i++){
             for(int j = 0; j<m; j++){
-                if(matrix[i][j] == mark){
+                if(row[i] == 1|| col[j] == 1){
                     matrix[i][j] = 0;
-            }
+                }
             }
         }
+        
     }
 };
