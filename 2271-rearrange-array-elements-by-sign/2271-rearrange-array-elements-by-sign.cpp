fd37eb0787c0917eb32b8fdc2d554a1a3624eac5
divya-36736 <23bec036@nith.ac.in>
2025-08-06 12:55:19 +0530

    Time: 0 ms (100%), Space: 128.3 MB (57.51%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
@@ -2,20 +2,33 @@ class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
         int n = nums.size();
-        vector<int>pos;
-        vector<int>neg;
+        // vector<int>pos;
+        // vector<int>neg;
+        // for(int i = 0; i<n; i++){
+        //     if(nums[i]>0){
+        //         pos.push_back(nums[i]);
+        //     }else{
+        //         neg.push_back(nums[i]);
+        // }
+        // }
+        // //pos at even index pr and neg at odd index pr
+        // for(int j = 0; j<n/2; j++){
+        //     nums[2*j] = pos[j];
+        //     nums[2*j + 1] = neg[j];
+        // }
+        // return nums;
+        int posInd = 0;
+        int negInd = 1;
+        vector<int>ans(n,0);
         for(int i = 0; i<n; i++){
-            if(nums[i]>0){
-                pos.push_back(nums[i]);
+            if(nums[i] > 0){
+                ans[posInd] = nums[i];
+                posInd+=2;
             }else{
-                neg.push_back(nums[i]);
-        }
-        }
-        //pos at even index pr and neg at odd index pr
-        for(int j = 0; j<n/2; j++){
-            nums[2*j] = pos[j];
-            nums[2*j + 1] = neg[j];
-        }
-        return nums;
+                ans[negInd] = nums[i];
+                negInd += 2;
+            }
+    }
+    return ans;
     }
 };
