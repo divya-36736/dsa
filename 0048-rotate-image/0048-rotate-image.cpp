5faae6f25c9bcc800cc2f7ec2c510d63d46199e4
divya-36736 <23bec036@nith.ac.in>
2025-08-07 13:42:41 +0530

    Time: 0 ms (100%), Space: 10.3 MB (35.32%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -4,16 +4,24 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
         
-        vector<vector<int>> rotated(n, vector<int>(m, 0));
-       for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                rotated[j][n - i - 1] = matrix[i][j];
+    //     vector<vector<int>> rotated(n, vector<int>(m, 0));
+    //    for (int i = 0; i < n; i++) {
+    //         for (int j = 0; j < m; j++) {
+    //             rotated[j][n - i - 1] = matrix[i][j];
+    //         }
+    //    }
+    //    for (int i = 0; i < n; i++) {
+    //         for (int j = 0; j < m; j++) {
+    //             matrix[i][j] = rotated[i][j];
+    //         }
+    //     }     
+        for(int i=0; i<n; i++){
+            for(int j = 0; j<i; j++){
+                swap(matrix[i][j], matrix[j][i]);
             }
-       }
-       for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                matrix[i][j] = rotated[i][j];
-            }
-        }      
+        }
+        for(int i=0; i<n; i++){
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
     }
 };
