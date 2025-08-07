8fc300731eaf4eddb65520857a04e470f69f3b6c
divya-36736 <23bec036@nith.ac.in>
2025-08-07 19:33:23 +0530

    Time: 0 ms (100%), Space: 12.6 MB (32.33%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -2,15 +2,39 @@ class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
         int n = nums.size();
-        int maxi = INT_MIN;
-        int ans = -1;
-        for(int i = 0; i<n; i++){
-            if((i == 0 || nums[i - 1] < nums[i])
-                && (i == n - 1 || nums[i] > nums[i + 1])){
-                ans = i;
-            }
+        //brute force approach
+
+        // int maxi = INT_MIN;
+        // int ans = -1;
+        // for(int i = 0; i<n; i++){
+        //     if((i == 0 || nums[i - 1] < nums[i])
+        //         && (i == n - 1 || nums[i] > nums[i + 1])){
+        //         ans = i;
+        //     }
             
+        // }
+        // return ans;
+
+        //optimal bs
+        if(n == 1) return 0;
+        if(nums[0]>nums[1]) return 0;
+        if(nums[n-1]>nums[n-2]) return n-1;
+
+        int low = 1;
+        int high = n-2;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            //1st we check at mid adjuscent
+            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
+                return mid;
+            }
+            else if(nums[mid]<nums[mid+1]){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
         }
-        return ans;
+        return -1;
     }
 };
