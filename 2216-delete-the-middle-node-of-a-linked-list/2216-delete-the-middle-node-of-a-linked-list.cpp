e3e084793df3efc46c71880a738af653301e5e89
divya-36736 <23bec036@nith.ac.in>
2025-08-08 21:24:18 +0530

    Time: 7 ms (11.78%), Space: 312 MB (55.06%) - LeetHub

--- 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.cpp
+++ 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.cpp
@@ -11,29 +11,48 @@
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
-        // brute approach
-        if(head == NULL || head->next == NULL) return NULL;
-        ListNode* temp = head;
-        int cnt = 0;
-        
-        while (temp != NULL) {
-            cnt++;
-            temp = temp->next;
-        }
-        // res define krenge for middle node
-        int res = cnt / 2;
-        temp = head;
-        while (temp != NULL) {
-            res--;
-            if (res == 0) {
-                ListNode* middle = temp->next;
-                temp->next = temp->next->next;
-                delete middle;
-                break;
-            }
-            temp = temp->next;
+        // // brute approach
+        // if(head == NULL || head->next == NULL) return NULL;
+        // ListNode* temp = head;
+        // int cnt = 0;
+
+        // while (temp != NULL) {
+        //     cnt++;
+        //     temp = temp->next;
+        // }
+        // // res define krenge for middle node
+        // int res = cnt / 2;
+        // temp = head;
+        // while (temp != NULL) {
+        //     res--;
+        //     if (res == 0) {
+        //         ListNode* middle = temp->next;
+        //         temp->next = temp->next->next;
+        //         delete middle;
+        //         break;
+        //     }
+        //     temp = temp->next;
+        // }
+
+        // return head;
+
+        if (head == NULL || head->next == NULL) {
+            return NULL;
         }
 
+        // Initialize slow and fast pointers
+        ListNode* slow = head;
+        ListNode* fast = head;
+        fast = head->next->next;
+
+        // Move 'fast' pointer twice as fast as 'slow'
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // Delete the middle node by skipping it
+        slow->next = slow->next->next;
         return head;
     }
 };
