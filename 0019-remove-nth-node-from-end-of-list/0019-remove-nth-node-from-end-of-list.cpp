df8877fff2e7414fe47aee27ec592e1e7bcb2092
divya-36736 <23bec036@nith.ac.in>
2025-08-08 21:18:11 +0530

    Time: 0 ms (100%), Space: 15 MB (59.31%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,44 +11,46 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        //brute force
-        if(head == NULL && head->next == NULL) return NULL;
+        // brute force
+        if (head == NULL && head->next == NULL)
+            return NULL;
         int cnt = 0;
         ListNode* temp = head;
-        while(temp != NULL){
+        while (temp != NULL) {
             cnt++;
             temp = temp->next;
         }
-        //if cnt == n matalb n head ko show  kr rha hai head ko delet rna hao
-        if(cnt == n){
+        // if cnt == n matalb n head ko show  kr rha hai head ko delet rna hao
+        if (cnt == n) {
             ListNode* newhead = head->next;
             delete head;
             return newhead;
         }
 
-        //ydi esa nhi hai toh postion of node to delete is (cnt - n+1) but hame ek variable set krna padega joh us node se pahale ruk jae
+        // ydi esa nhi hai toh postion of node to delete is (cnt - n+1) but hame
+        // ek variable set krna padega joh us node se pahale ruk jae
         int res = cnt - n;
         temp = head;
-        while(temp != NULL){
+        while (temp != NULL) {
             res--;
-            if(res == 0){
+            if (res == 0) {
+                ListNode* delnode = temp->next;
+                temp->next = temp->next->next;
+                delete delnode;
                 break;
             }
             temp = temp->next;
         }
 
-        ListNode* delnode = temp->next;
-        temp->next = temp->next->next;
-        delete delnode;
         return head;
 
-        //optimal;
-        // ListNode* fastp = head;
-        // ListNode* slowp = head;
+        // optimal;
+        //  ListNode* fastp = head;
+        //  ListNode* slowp = head;
 
-        // // Move the fastp pointer N nodes ahead
-        // for (int i = 0; i < n; i++)
-        //     fastp = fastp->next;
+        // Move the fastp pointer N nodes ahead
+        //  for (int i = 0; i < n; i++)
+        //      fastp = fastp->next;
 
         // // If fastp becomes NULL,
         // // the Nth node from the end is the head
