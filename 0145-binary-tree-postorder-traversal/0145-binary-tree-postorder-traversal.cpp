4b7a01bdfafe74bc2eaed6bafe609768c1bb7967
divya-36736 <23bec036@nith.ac.in>
2025-08-09 12:27:32 +0530

    Time: 0 ms (100%), Space: 10.9 MB (71.47%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -6,7 +6,8 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
@@ -23,24 +24,53 @@ public:
         // vector<int> res;
         // postorder(root, res);
         // return res;
-        vector<int>postorder;
-        stack<TreeNode*>st1, st2;
-        if(root == NULL) return postorder;
-        //1st push root
-        st1.push(root);
-        while(!st1.empty()){
-            root = st1.top();
-            st1.pop();
-            st2.push(root);
+        // //iterative approach using 2 stack
+        // vector<int>postorder;
+        // stack<TreeNode*>st1, st2;
+        // if(root == NULL) return postorder;
+        // //1st push root
+        // st1.push(root);
+        // while(!st1.empty()){
+        //     root = st1.top();
+        //     st1.pop();
+        //     st2.push(root);
 
-            //left child
-            if(root->left != NULL) st1.push(root->left);
-            //right childe
-            if(root->right != NULL) st1.push(root->right);
-        }
-        while(!st2.empty()){
-            postorder.push_back(st2.top()->val);
-            st2.pop();
+        //     //left child
+        //     if(root->left != NULL) st1.push(root->left);
+        //     //right childe
+        //     if(root->right != NULL) st1.push(root->right);
+        // }
+        // while(!st2.empty()){
+        //     postorder.push_back(st2.top()->val);
+        //     st2.pop();
+        // }
+        // return postorder;
+
+        // iterative approach using one stack
+        vector<int> postorder;
+        if (root == NULL)
+            return postorder;
+
+        stack<TreeNode*> st;
+        TreeNode* curr = root;
+        TreeNode* lastVisited = NULL; // To track last processed node
+
+        while (curr != NULL || !st.empty()) {
+            // Go as left as possible
+            if (curr != NULL) {
+                st.push(curr);
+                curr = curr->left;
+            } else {
+                TreeNode* node = st.top();
+                // If right child exists and hasn't been visited yet
+                if (node->right != NULL && lastVisited != node->right) {
+                    curr = node->right;
+                } else {
+                    postorder.push_back(node->val);
+                    lastVisited = node;
+                    st.pop();
+                }
+            }
         }
         return postorder;
     }
