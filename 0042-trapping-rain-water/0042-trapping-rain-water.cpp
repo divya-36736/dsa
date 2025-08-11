b6327a3686c510cab7af81358295b59cc700bbc7
divya-36736 <23bec036@nith.ac.in>
2025-08-11 21:55:04 +0530

    Time: 0 ms (100%), Space: 26 MB (60.12%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,28 +1,50 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        //optimal aaproach
+        // optimal aaproach
         int n = height.size();
-        vector<int>lmax(n,0);
-        vector<int>rmax(n,0);
-       
-        lmax[0] = height[0];
-        rmax[n-1] = height[n-1];
-        //for left
-        for(int i = 1; i<n; i++){
-            lmax[i] = max(lmax[i-1], height[i]);
-        }
-        //for right
-        for(int i = n-2; i>=0; i--){
-            rmax[i] = max(rmax[i+1], height[i]);
+        // vector<int>lmax(n,0);
+        // vector<int>rmax(n,0);
+
+        // lmax[0] = height[0];
+        // rmax[n-1] = height[n-1];
+        // //for left
+        // for(int i = 1; i<n; i++){
+        //     lmax[i] = max(lmax[i-1], height[i]);
+        // }
+        // //for right
+        // for(int i = n-2; i>=0; i--){
+        //     rmax[i] = max(rmax[i+1], height[i]);
+        // }
+
+        // int ans = 0;
+        // int mini = 0;
+        // for(int i = 0; i<n; i++){
+        //     mini = min(lmax[i], rmax[i]);
+        //     ans += mini - height[i];
+        // }
+        // return ans;
+
+        // 2 pointer approach
+        int l = 0;
+        int r = n - 1;
+        int lmax = 0;
+        int rmax = 0;
+        int ans = 0;
+        while (l < r) {
+            lmax = max(lmax, height[l]);
+            rmax = max(rmax, height[r]);
+
+            // check
+            if (lmax < rmax) {
+                ans += lmax - height[l];
+                l++;
+            } else {
+                ans += rmax - height[r];
+                r--;
+            }
         }
 
-        int ans = 0;
-        int mini = 0;
-        for(int i = 0; i<n; i++){
-            mini = min(lmax[i], rmax[i]);
-            ans += mini - height[i];
-        }
         return ans;
     }
 };
