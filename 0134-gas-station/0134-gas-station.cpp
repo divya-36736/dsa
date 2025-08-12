f944a6b606e8c6dea8436e9dca77fcd5f514ce0b
divya-36736 <23bec036@nith.ac.in>
2025-08-12 18:34:45 +0530

    Time: 4 ms (24.68%), Space: 112.4 MB (40.43%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -6,20 +6,25 @@ public:
         //total gas
         int totgas = 0;
         int totcost = 0;
-        for(int val:gas){
-            totgas += val;
-        }
-        //total cost
-        for(int val: cost){
-            totcost += val;
-        }
-        //if this fail
-        if(totgas < totcost){
-            return -1;
-        }
+        // for(int val:gas){
+        //     totgas += val;
+        // }
+        // //total cost
+        // for(int val: cost){
+        //     totcost += val;
+        // }
+        // //if this fail
+        // if(totgas < totcost){
+        //     return -1;
+        // } 
+        //in above 2 different loops use krene ki bjay 
+        //hum sara ek hi loop me kr dakte hai 
+
         int st = 0;
         int currgas = 0;
         for(int i = 0; i<g; i++){
+            totgas+=gas[i];
+            totcost += cost[i];
             currgas += (gas[i]-cost[i]);
             if(currgas<0){
                 st = i+1;
@@ -27,6 +32,19 @@ public:
             }
         }
 
-        return st;
+        return totgas < totcost? -1:st;
+
+
+        // int st = 0;
+        // int currgas = 0;
+        // for(int i = 0; i<g; i++){
+        //     currgas += (gas[i]-cost[i]);
+        //     if(currgas<0){
+        //         st = i+1;
+        //         currgas = 0;
+        //     }
+        // }
+
+        // return st;
     }
 };
