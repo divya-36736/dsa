3dbe39d669d0cd944a078a6c908b264b764e865b
divya-36736 <23bec036@nith.ac.in>
2025-08-12 09:29:08 +0530

    Time: 0 ms (100%), Space: 10.8 MB (63.21%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -26,27 +26,58 @@ public:
         // inorder(root, result);
         // return result;
 
-        // iterative method left root r
-        vector<int> inorder;   // Stores the result
-        stack<TreeNode*> st;   // Stack for simulation
-        TreeNode* curr = root; // Start from root
+        // // iterative method left root r
+        // vector<int> inorder;   // Stores the result
+        // stack<TreeNode*> st;   // Stack for simulation
+        // TreeNode* curr = root; // Start from root
 
-        while (curr != NULL || !st.empty()) {
-            // Step 1: Go to the leftmost node
-            while (curr != NULL) {
-                st.push(curr);
-                curr = curr->left;
+        // while (curr != NULL || !st.empty()) {
+        //     // Step 1: Go to the leftmost node
+        //     while (curr != NULL) {
+        //         st.push(curr);
+        //         curr = curr->left;
+        //     }
+
+        //     // Step 2: Process top of stack
+        //     curr = st.top();
+        //     st.pop();
+        //     inorder.push_back(curr->val);
+
+        //     // Step 3: Go to right subtree
+        //     curr = curr->right;
+        // }
+
+        // return inorder;
+
+        // morris inorder traversal
+
+        vector<int> ans;
+        TreeNode* curr = root;
+
+        while (curr != NULL) {
+            if (curr->left == NULL) {
+                ans.push_back(curr->val);
+                curr = curr->right;
+            } else {
+                // Find the inorder predecessor
+                TreeNode* IP = curr->left;
+                while (IP->right != NULL && IP->right != curr) {
+                    IP = IP->right;
+                }
+
+                if (IP->right == NULL) {
+                    // Make temporary thread
+                    IP->right = curr;
+                    curr = curr->left;
+                } else {
+                    // Remove thread and visit node
+                    IP->right = NULL;
+                    ans.push_back(curr->val);
+                    curr = curr->right;
+                }
             }
-
-            // Step 2: Process top of stack
-            curr = st.top();
-            st.pop();
-            inorder.push_back(curr->val);
-
-            // Step 3: Go to right subtree
-            curr = curr->right;
         }
 
-        return inorder;
+        return ans;
     }
 };
