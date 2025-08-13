732c69af862000ec934edf7a662448182e7d72f4
divya-36736 <23bec036@nith.ac.in>
2025-08-13 19:06:22 +0530

    Time: 6 ms (25.2%), Space: 9.6 MB (90.86%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int s1 = s.size();
-        int t1 = t.size();
-        unordered_map<char, int>count1, count2;
-        if(s1 != t1) return false;
-        for(int i = 0; i<s1; i++){
-            count1[s[i]]++; //characters store in map
-            count2[t[i]]++;
-        }
+    //     //if total counts of per char is equal in both 
+    //     //then true otherwise faLSE
+    //     int s1 = s.size();
+    //     int t1 = t.size();
+    //     unordered_map<char, int>count1, count2;
+    //     if(s1 != t1) return false;
+    //     for(int i = 0; i<s1; i++){
+    //         count1[s[i]]++; //characters store in map
+    //         count2[t[i]]++;
+    //     }
+    //     //only check counts if equal then true
+    //    return count1 == count2;
 
-       return count1 == count2;
+    sort(s.begin(), s.end());
+    sort(t.begin(), t.end());
+    if(s.size() != t.size())  return false;
+    for(int i = 0; i<s.size(); i++){
+        if(s[i] != t[i]){
+            return false;
+        }
+    }
+    return true;
     }
 };
