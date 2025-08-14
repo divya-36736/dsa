20f5807066d966b669bce5651571e5d172c8bf22
divya-36736 <23bec036@nith.ac.in>
2025-08-14 22:39:50 +0530

    Time: 0 ms (100%), Space: 22.9 MB (61.77%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -2,31 +2,57 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        vector<int> left(n), right(n);
-        //leftmost and rightmost should be 1
+        // vector<int> left(n), right(n);
+        // //leftmost and rightmost should be 1
+        // left[0] = 1;
+        // right[n - 1] = 1;
+        // // Left to right pass
+        // for (int i = 1; i < n; i++) {
+        //     if (ratings[i] > ratings[i - 1]) {
+        //         left[i] = left[i - 1] + 1;
+        //     } else {
+        //         left[i] = 1;
+        //     }
+        // }
+        // // Right to left pass
+        // for (int i = n - 2; i >= 0; i--) {
+        //     if (ratings[i] > ratings[i + 1]) {
+        //         right[i] = right[i + 1] + 1;
+        //     } else {
+        //         right[i] = 1;
+        //     }
+        // }
+        // // Calculate total candies
+        // int sum = 0;
+        // for (int i = 0; i < n; i++) {
+        //     sum += max(left[i], right[i]);
+        // }
+        // return sum;
+
+        //optimal 2nd approach
+        //right vector remove
+        vector<int>left(n);
         left[0] = 1;
-        right[n - 1] = 1;
-        // Left to right pass
-        for (int i = 1; i < n; i++) {
-            if (ratings[i] > ratings[i - 1]) {
-                left[i] = left[i - 1] + 1;
-            } else {
+        for(int i = 1; i<n; i++){
+            if(ratings[i] > ratings[i-1]){
+                left[i] = left[i-1]+1;
+            }
+            else{
                 left[i] = 1;
             }
         }
-        // Right to left pass
-        for (int i = n - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1]) {
-                right[i] = right[i + 1] + 1;
-            } else {
-                right[i] = 1;
+        int curr = 1;
+        int sum = max(left[n - 1], curr);
+        for(int i = n-2; i>=0; i--){
+            if(ratings[i] > ratings[i+1]){
+                curr += 1;
             }
-        }
-        // Calculate total candies
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += max(left[i], right[i]);
-        }
+            else{
+                curr =1; 
+            }
+            sum += max(left[i], curr);
+        } 
+        
         return sum;
     }
 };
