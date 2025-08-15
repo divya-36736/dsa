74cef0215b71799c96dad69dc7b07c8d18f2e5f2
divya-36736 <23bec036@nith.ac.in>
2025-08-15 19:18:32 +0530

    Time: 0 ms (100%), Space: 16.1 MB (76.87%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -24,26 +24,51 @@ public:
         //tabulation
         //1st dp 2d vector
         
-        vector<vector<int>>dp(n, vector<int>(m, 0));
+        // vector<vector<int>>dp(n, vector<int>(m, 0));
+        // for(int i = 0; i<n; i++){
+        //     for(int j = 0; j<m; j++){
+        //         //base case 
+        //         if(i == 0 && j==0){
+        //            dp[i][j] =  grid[i][j];
+        //            continue;
+        //         }
+        //         //now check in up and left
+        //         int up = grid[i][j];
+        //         if(i>0) up += dp[i-1][j];
+        //         else up += 1e9;
+        //         //now in left
+        //         int left = grid[i][j];
+        //         if(j>0) left += dp[i][j-1];
+        //         else left += 1e9; 
+
+        //         dp[i][j] = min(up, left);
+        //     }
+        // }
+        // return dp[n-1][m-1];
+
+        //space optimized
+        vector<int>prev(m, 0); //represent prev row
         for(int i = 0; i<n; i++){
+            vector<int>temp(m, 0); //store curr row
             for(int j = 0; j<m; j++){
                 //base case 
                 if(i == 0 && j==0){
-                   dp[i][j] =  grid[i][j];
+                   temp[j] =  grid[i][j];
                    continue;
                 }
                 //now check in up and left
                 int up = grid[i][j];
-                if(i>0) up += dp[i-1][j];
+                if(i>0) up += prev[j];
                 else up += 1e9;
                 //now in left
                 int left = grid[i][j];
-                if(j>0) left += dp[i][j-1];
+                if(j>0) left += temp[j-1];
                 else left += 1e9; 
 
-                dp[i][j] = min(up, left);
+                temp[j] = min(up, left);
             }
+            prev = temp; //update prev after finishing the row
         }
-        return dp[n-1][m-1];
+        return prev[m-1];
     }
 };
