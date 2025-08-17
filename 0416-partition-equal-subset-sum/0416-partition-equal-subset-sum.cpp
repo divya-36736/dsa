78399139be42f7f623b3d7cafd18046cdc172cd0
divya-36736 <23bec036@nith.ac.in>
2025-08-18 00:14:57 +0530

    Time: 139 ms (46%), Space: 16.7 MB (76.49%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -25,6 +25,44 @@ public:
     bool canPartition(vector<int>& nums) {
         // size of the array
         int n = nums.size();
+        // int totsum = 0;
+        // for (int i = 0; i < n; i++) { // totalsum
+        //     totsum += nums[i];
+        // }
+        // if (totsum % 2 != 0)
+        //     return false; // not even
+        // else {            // if even
+        //     int k = (totsum / 2);
+        //     // vector<vector<int>>dp(n, vector<int>(k+1, -1));
+
+        //     // return solve(n-1, k, dp, nums);
+
+        //     // tabulation
+        //     vector<vector<bool>> dp(n, vector<bool>(k + 1,false));
+        //     // base case
+        //     for (int i = 0; i < n; i++) {
+        //         dp[i][0] = true;
+        //     }
+        //     if (nums[0] <= k)
+        //         dp[0][nums[0]] = true;
+        //     // loop
+        //     for (int i = 1; i < n; i++) {
+        //         for (int target = 0; target<=k; target++) {
+        //             // not take
+        //             bool nottake = dp[i-1][target];
+
+        //             // take
+        //             bool take = false;
+        //             if (nums[i] <= target) {
+        //                 take = dp[i-1][target-nums[i]];
+        //             }
+        //             dp[i][target] = take || nottake;
+        //         }
+        //     }
+        //     return dp[n-1][k];
+        // }
+
+        // space optimization
         int totsum = 0;
         for (int i = 0; i < n; i++) { // totalsum
             totsum += nums[i];
@@ -38,28 +76,28 @@ public:
             // return solve(n-1, k, dp, nums);
 
             // tabulation
-            vector<vector<bool>> dp(n, vector<bool>(k + 1,false));
+            vector<bool>prev(k+1, false); //prev row value store
             // base case
-            for (int i = 0; i < n; i++) {
-                dp[i][0] = true;
-            }
+            prev[0] = true;
             if (nums[0] <= k)
-                dp[0][nums[0]] = true;
+                prev[nums[0]] = true;
             // loop
             for (int i = 1; i < n; i++) {
-                for (int target = 0; target<=k; target++) {
+                vector<bool>curr(k+1, false); //curr row value store
+                for (int target = 0; target <= k; target++) {
                     // not take
-                    bool nottake = dp[i-1][target];
+                    bool nottake = prev[target];
 
                     // take
                     bool take = false;
                     if (nums[i] <= target) {
-                        take = dp[i-1][target-nums[i]];
+                        take = prev[target - nums[i]];
                     }
-                    dp[i][target] = take || nottake;
+                    curr[target] = take || nottake;
                 }
+                prev = curr; // ✅ update
             }
-            return dp[n-1][k]; 
-        } 
+            return prev[k];
+        }
     }
 };
