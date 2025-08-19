c47a135911a64222a22518b9c9833f50ce26d47e
divya-36736 <23bec036@nith.ac.in>
2025-08-19 09:49:17 +0530

    Time: 51 ms (25.8%), Space: 48.8 MB (44.05%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -20,9 +20,41 @@ public:
     }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>>dp(n, vector<int>(amount+1, -1));
-        int ans = minimum(n-1, amount, coins, dp);
-        if(ans>= 1e9) return -1;
-        return ans;
+        // vector<vector<int>>dp(n, vector<int>(amount+1, -1));
+        // int ans = minimum(n-1, amount, coins, dp);
+        // if(ans>= 1e9) return -1;
+        // return ans;
+
+        //tabulation
+        //make vector same size
+        vector<vector<int>>dp(n, vector<int>(amount+1, 0));
+        //base case
+        //target ki kisi bhi value ke liye 
+        //if (target%coins[0] == 0) hai toh 
+        //dp[0][i] = i / arr[0]; else dp[0][i] = 1e9
+        for(int i = 0; i<=amount; i++){
+            if(i%coins[0] == 0){
+                dp[0][i] = i/coins[0];
+            }
+            else dp[0][i] = 1e9;
+        }
+        //for loop
+        for(int i = 1; i<n; i++){
+            for(int target = 0; target<=amount; target++){
+                int nottake = 0+dp[i-1][target];
+                //take
+                int take = 1e9;
+                if(coins[i]<=target){
+                take = 1+ dp[i][target-coins[i]];
+                } 
+                dp[i][target] = min(take, nottake);
+            }
+            
+        }
+        int ans = dp[n-1][amount];
+            if(ans == 1e9){
+                return -1;
+            }
+            return ans;
     }
 };
