64a8906b84b00f42b3e1b942f634e8c3c6f8d8d6
divya-36736 <23bec036@nith.ac.in>
2025-08-25 16:31:39 +0530

    Time: 21 ms (9.79%), Space: 23.1 MB (8.33%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -3,28 +3,49 @@ public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
         int n = matrix.size();
         int m = matrix[0].size();
-        //1st we create min-heap
-        vector<pair<int,pair<int, int>>>temp;
-        for(int i = 0; i<n; i++){
-            temp.push_back(make_pair(matrix[i][0], make_pair(i, 0)));
-        }
-        priority_queue<pair<int,pair<int, int>>,
-        vector<pair<int, pair<int, int>>>,
-        greater<pair<int, pair<int, int>>>>p(temp.begin(), temp.end());
+        // 1st we create min-heap
+        //  vector<pair<int,pair<int, int>>>temp;
+        //  for(int i = 0; i<n; i++){
+        //      temp.push_back(make_pair(matrix[i][0], make_pair(i, 0)));
+        //  }
+        //  priority_queue<pair<int,pair<int, int>>,
+        //  vector<pair<int, pair<int, int>>>,
+        //  greater<pair<int, pair<int, int>>>>p(temp.begin(), temp.end());
 
-        int ans;
-        pair<int, pair<int, int>>element;
-        int i, j;
-        while(k--){
-            element = p.top();
-            p.pop();
-            ans = element.first;
-            i = element.second.first;
-            j = element.second.second;
+        // int ans;
+        // pair<int, pair<int, int>>element;
+        // int i, j;
+        // while(k--){
+        //     element = p.top();
+        //     p.pop();
+        //     ans = element.first;
+        //     i = element.second.first;
+        //     j = element.second.second;
 
-            if(j+1 < n){
-                p.push(make_pair(matrix[i][j+1], make_pair(i, j+1)));
+        //     if(j+1 < n){
+        //         p.push(make_pair(matrix[i][j+1], make_pair(i, j+1)));
+        //     }
+        // }
+        // return ans;
+
+        vector<pair<int, pair<int, int>>> temp;
+        for (int i = 0; i < n; i++) {
+            for(int j = 0; j<m; j++){
+                temp.push_back(make_pair(matrix[i][j], make_pair(i, j)));
             }
+        }
+        priority_queue<pair<int, pair<int, int>>,
+                       vector<pair<int, pair<int, int>>>,
+                       greater<pair<int, pair<int, int>>>>
+            p(temp.begin(), temp.end());
+
+        pair<int, pair<int, int>> element;
+        int ans;
+        while (k > 0 && !p.empty()) {
+            k--;
+            element = p.top();
+            ans = element.first;
+            p.pop();
         }
         return ans;
     }
