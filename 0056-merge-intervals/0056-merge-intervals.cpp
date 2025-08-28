71e9b8200c286aced0eae9e2bba02f87a818f849
divya-36736 <23bec036@nith.ac.in>
2025-08-28 23:29:00 +0530

    Time: 2 ms (93.25%), Space: 23.7 MB (94.09%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -2,20 +2,43 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         int n = intervals.size();
-        vector<vector<int>>res;
         sort(intervals.begin(), intervals.end());
-        if(n == 0) return res;
+        //in this we need to check all iteration
+        // vector<vector<int>>res;
+        
+        // if(n == 0) return res;
 
-        vector<int>tempInt = intervals[0]; //1st push
-        for(auto it: intervals){
-            if(it[0] <= tempInt[1]){
-                tempInt[1] = max(it[1], tempInt[1]);
-            }else{
-                res.push_back(tempInt);
-                tempInt = it;
+        // vector<int>tempInt = intervals[0]; //1st push
+
+        // for(auto it: intervals){
+        //     if(it[0] <= tempInt[1]){ //
+        //         tempInt[1] = max(it[1], tempInt[1]);
+        //     }else{
+        //         res.push_back(tempInt);
+        //         tempInt = it;
+        //     }
+        // }
+        // res.push_back(tempInt);
+        // return res;
+
+        //2nd
+        vector<vector<int>>res;
+        int i = 0;
+
+        while (i < n) {
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+
+            // ✅ Merge all overlapping intervals
+            while (i + 1 < n && intervals[i + 1][0] <= end) {
+                end = max(end, intervals[i + 1][1]);
+                i++;
             }
+
+            res.push_back({start, end});
+            i++;
         }
-        res.push_back(tempInt);
+
         return res;
     }
 };
