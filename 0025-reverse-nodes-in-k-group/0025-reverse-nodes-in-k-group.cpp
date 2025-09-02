53419a83c2ea2623fe0ad35bcd62c8845ac53471
divya-36736 <23bec036@nith.ac.in>
2025-09-02 12:00:25 +0530

    Time: 0 ms (100%), Space: 16.5 MB (70.63%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -10,26 +10,69 @@
  */
 class Solution {
 public:
+    ListNode* reverse(ListNode* head){
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        while(curr){
+            ListNode* temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+
+    ListNode* getkthNode(ListNode* head, int k){
+        k -= 1;
+        ListNode* temp  = head;
+        while(temp != NULL && k>0){
+            k--;
+            temp = temp->next;
+        }
+        return temp;
+    }
+
     ListNode* reverseKGroup(ListNode* head, int k) {
         ListNode* temp = head;
-        int cnt = 0;
-        while(cnt < k){
-            if(temp == NULL){
-                return head;
+        ListNode* prevLast =NULL;
+        while(temp != NULL){
+            ListNode* kthNode = getkthNode(temp , k);
+            if(kthNode == NULL){
+                if(prevLast) prevLast->next = temp;
+                break;
             }
-            temp = temp ->next;
-            cnt++;
+            ListNode* nextNode = kthNode->next;
+            kthNode->next = NULL;
+            reverse(temp);
+            if(temp == head){
+                head = kthNode;
+            }else{
+                prevLast->next = kthNode;
+            }
+            prevLast = temp;
+            temp = nextNode;
+        
         }
-        ListNode* prevNode = reverseKGroup(temp, k);
-        temp = head;
-        cnt = 0;
-        while(cnt<k){
-            ListNode* next = temp->next;
-            temp->next = prevNode;
-            prevNode = temp;
-            temp = next;
-            cnt++;
-        }
-        return prevNode;
+        return head;
+        // ListNode* temp = head;
+        // int cnt = 0;
+        // while(cnt < k){
+        //     if(temp == NULL){
+        //         return head;
+        //     }
+        //     temp = temp ->next;
+        //     cnt++;
+        // }
+        // ListNode* prevNode = reverseKGroup(temp, k);
+        // temp = head;
+        // cnt = 0;
+        // while(cnt<k){
+        //     ListNode* next = temp->next;
+        //     temp->next = prevNode;
+        //     prevNode = temp;
+        //     temp = next;
+        //     cnt++;
+        // }
+        // return prevNode;
     }
 };
