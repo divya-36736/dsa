6648f38108d4ffb41aa5d908970319d0b099ca39
divya-36736 <23bec036@nith.ac.in>
2025-09-03 10:26:02 +0530

    Time: 7 ms (69.63%), Space: 26.2 MB (15.73%) - LeetHub

--- 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.cpp
+++ 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,65 +1,90 @@
+// class Solution {
+// public:
+
+//     int sumByD(vector<int>& nums, int div){
+//         int n = nums.size();
+        
+//         int sum = 0;
+//         for(int i = 0; i<n; i++){
+//             sum += ceil((double)nums[i]/(double)div);
+//         }
+//         return sum;
+//     }
+
+//     int smallestDivisor(vector<int>& nums, int threshold) {
+//         int n = nums.size();
+//         if(n>threshold) return -1;
+//         int low = 1;
+//         int high = *max_element(nums.begin(), nums.end());
+//         while(low <= high){
+//             int mid = low + (high-low)/2;
+//             if (sumByD(nums, mid) <= threshold) {
+//                 high = mid-1;
+//             } else {
+//                 low = mid + 1; 
+//             }
+//         }
+//         return low;
+
+//         //Brute approach
+//         // int maxi = *max_element(nums.begin(), nums.end());
+//         // for(int d = 1; d<=maxi; d++){
+//         //     int sum = 0;
+//         //     for(int i = 0; i<n; i++){
+//         //         sum += ceil((double)nums[i]/(double)d);
+//         //     }
+//         //     if(sum <= threshold){
+//         //         return d;
+//         //         break;
+//         //     }
+//         // }
+//         // return maxi;
+
+//         //optimal approach
+//         // int maxi = *max_element(nums.begin(), nums.end());
+//         // int st = 1;
+//         // int end = maxi;
+//         // while(st<end){
+//         //     int mid = st + (end-st)/2;
+//         //     int sum = 0;
+//         //     for(int i = 0; i<n; i++){
+//         //         sum += ceil((double)nums[i]/(double)mid);
+//         //     }
+//         //     if(sum <= threshold){
+//         //         end = mid;
+//         //     }
+//         //     else{
+//         //         st = mid+1;
+//         //     }
+//         // }
+//         // return st;
+
+        
+        
+//     }
+// };
 class Solution {
 public:
-
-    int sumByD(vector<int>& nums, int div){
-        int n = nums.size();
-        
-        int sum = 0;
-        for(int i = 0; i<n; i++){
-            sum += ceil((double)nums[i]/(double)div);
+    static long long computeSum(const vector<int>& nums, int d) {
+        long long total = 0;
+        for (int num : nums) {
+            total += (num + d - 1) / d; // integer ceil
         }
-        return sum;
+        return total;
     }
 
     int smallestDivisor(vector<int>& nums, int threshold) {
-        int n = nums.size();
-        if(n>threshold) return -1;
         int low = 1;
         int high = *max_element(nums.begin(), nums.end());
-        while(low <= high){
-            int mid = low + (high-low)/2;
-            if (sumByD(nums, mid) <= threshold) {
-                high = mid-1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (computeSum(nums, mid) <= threshold) {
+                high = mid;      // feasible, try smaller
             } else {
-                low = mid + 1; 
+                low = mid + 1;   // too small, need bigger divisor
             }
         }
         return low;
-
-        //Brute approach
-        // int maxi = *max_element(nums.begin(), nums.end());
-        // for(int d = 1; d<=maxi; d++){
-        //     int sum = 0;
-        //     for(int i = 0; i<n; i++){
-        //         sum += ceil((double)nums[i]/(double)d);
-        //     }
-        //     if(sum <= threshold){
-        //         return d;
-        //         break;
-        //     }
-        // }
-        // return maxi;
-
-        //optimal approach
-        // int maxi = *max_element(nums.begin(), nums.end());
-        // int st = 1;
-        // int end = maxi;
-        // while(st<end){
-        //     int mid = st + (end-st)/2;
-        //     int sum = 0;
-        //     for(int i = 0; i<n; i++){
-        //         sum += ceil((double)nums[i]/(double)mid);
-        //     }
-        //     if(sum <= threshold){
-        //         end = mid;
-        //     }
-        //     else{
-        //         st = mid+1;
-        //     }
-        // }
-        // return st;
-
-        
-        
     }
 };
