f9126966aa379df1ea43ae746aa3a705752d646d
divya-36736 <23bec036@nith.ac.in>
2025-09-05 13:50:54 +0530

    Time: 3 ms (74.42%), Space: 15.4 MB (66.57%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
@@ -2,16 +2,65 @@ class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
         int n = nums.size();
-        long long sum = 0;
-        for(int i = 0; i<n; i++){
-            int mini = INT_MAX;
-            int maxi = INT_MIN;
-            for(int j = i; j<n; j++){
-                mini = min(mini, nums[j]);
-                maxi = max(maxi, nums[j]);
-                sum += abs(maxi - mini);
-            }
+        // //bruteforce approach
+        // long long sum = 0;
+        // for(int i = 0; i<n; i++){
+        //     int mini = INT_MAX;
+        //     int maxi = INT_MIN;
+        //     for(int j = i; j<n; j++){
+        //         mini = min(mini, nums[j]);
+        //         maxi = max(maxi, nums[j]);
+        //         sum += abs(maxi - mini);
+        //     }
+        // }
+        // return sum;
+
+        //optimal
+        vector<int> pse(n), nse(n), pge(n), nge(n);
+        stack<int> st;
+
+        // Previous Smaller (strictly smaller)
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && nums[st.top()] > nums[i]) st.pop();
+            pse[i] = st.empty() ? (i + 1) : (i - st.top());
+            st.push(i);
         }
-        return sum;
+
+        while (!st.empty()) st.pop();
+
+        // Next Smaller (strictly smaller OR equal to handle duplicates)
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && nums[st.top()] >= nums[i]) st.pop();
+            nse[i] = st.empty() ? (n - i) : (st.top() - i);
+            st.push(i);
+        }
+
+        while (!st.empty()) st.pop();
+
+        // Previous Greater (strictly greater)
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && nums[st.top()] < nums[i]) st.pop();
+            pge[i] = st.empty() ? (i + 1) : (i - st.top());
+            st.push(i);
+        }
+
+        while (!st.empty()) st.pop();
+
+        // Next Greater (strictly greater OR equal to handle duplicates)
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && nums[st.top()] <= nums[i]) st.pop();
+            nge[i] = st.empty() ? (n - i) : (st.top() - i);
+            st.push(i);
+        }
+
+        long long res = 0;
+
+        for (int i = 0; i < n; i++) {
+            long long maxContribution = 1LL * nums[i] * pge[i] * nge[i];
+            long long minContribution = 1LL * nums[i] * pse[i] * nse[i];
+            res += (maxContribution - minContribution);
+        }
+
+        return res;
     }
 };
