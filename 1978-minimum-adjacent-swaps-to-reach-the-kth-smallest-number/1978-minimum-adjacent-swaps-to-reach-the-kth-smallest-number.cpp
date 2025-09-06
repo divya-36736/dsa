ebcc0bd57765f6fe6a6aaf01fb0f9b9c45f4f6f1
divya-36736 <23bec036@nith.ac.in>
2025-09-06 10:19:01 +0530

    Time: 21 ms (56.63%), Space: 9.4 MB (27.11%) - LeetHub

--- 1978-minimum-adjacent-swaps-to-reach-the-kth-smallest-number/1978-minimum-adjacent-swaps-to-reach-the-kth-smallest-number.cpp
+++ 1978-minimum-adjacent-swaps-to-reach-the-kth-smallest-number/1978-minimum-adjacent-swaps-to-reach-the-kth-smallest-number.cpp
@@ -1,41 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
-    void nextPermutation(string &s) {
-        int n = s.size();
-        int i = n - 2;
-        while (i >= 0 && s[i] >= s[i + 1]) i--;
-        if (i >= 0) {
-            int j = n - 1;
-            while (s[j] <= s[i]) j--;
-            swap(s[i], s[j]);
-        }
-        reverse(s.begin() + i + 1, s.end());
-    }
-
     int getMinSwaps(string num, int k) {
         string target = num;
-        for (int i = 0; i < k; i++) {
-            nextPermutation(target);
+
+        // generate kth next permutation directly using STL
+        while (k--) {
+            next_permutation(target.begin(), target.end());
         }
 
         int n = num.size();
         int swaps = 0;
 
+        // count minimum adjacent swaps
         for (int i = 0; i < n; i++) {
             if (num[i] == target[i]) continue;
 
-            // find position of target[i] in num (starting from i+1)
             int j = i + 1;
             while (num[j] != target[i]) j++;
 
-            // bubble num[j] leftwards to position i
             while (j > i) {
                 swap(num[j], num[j - 1]);
                 swaps++;
                 j--;
             }
         }
-
         return swaps;
     }
 };
