add520024e51b8751100fb850c390acca59f8599
divya-36736 <23bec036@nith.ac.in>
2025-09-11 01:09:46 +0530

    Time: 0 ms (100%), Space: 21.8 MB (71.34%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        bool rows[9][9] = {false};
+        bool cols[9][9] = {false};
+        bool boxes[9][9] = {false};
+
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                if(board[i][j] != '.'){
+                    int num = board[i][j] - '1';
+                    int boxInd = (i/3) * 3 + (j/3);
+
+                    if(rows[i][num] || cols[j][num] || boxes[boxInd][num])
+                        return false;
+
+                    rows[i][num] = cols[j][num] = boxes[boxInd][num] = true;
+                }
+            }
+        }
+        return true;
+    }
+};
