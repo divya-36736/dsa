1d67cef21c37df0e8b5411de4086e982b4f3a155
divya-36736 <23bec036@nith.ac.in>
2025-09-16 10:27:30 +0530

    Time: 2 ms (21.42%), Space: 8.7 MB (14.69%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,14 +1,34 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if(x == 0 || x == 1) return x;
-        int ans  = 1;
-        for(int i = 1; i<=x/2; i++){
-            long long sq = 1LL*i*i;
-            if(sq == x) return i;
-            if(sq > x) return i-1;
-            ans = i;
-        } 
+        // if(x == 0 || x == 1) return x;
+        // int ans  = 1;
+        // for(int i = 1; i<=x/2; i++){
+        //     long long sq = 1LL*i*i;
+        //     if(sq == x) return i;
+        //     if(sq > x) return i-1;
+        //     ans = i;
+        // }
+        // return ans;
+
+        // optimal
+        if (x == 0 || x == 1)
+            return x;
+
+        int left = 1, right = x, ans = 0;
+        while (left <= right) {
+            long long mid = left + (right - left) / 2; // avoid overflow
+            long long sq = mid * mid;
+
+            if (sq == x)
+                return mid;
+            else if (sq < x) {
+                ans = mid; // mid is a candidate
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
         return ans;
     }
 };
