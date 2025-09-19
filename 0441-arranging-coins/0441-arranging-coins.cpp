dde7f90ed80f8491bdb6a3a9151f57d13ac5abb1
divya-36736 <23bec036@nith.ac.in>
2025-09-19 18:00:31 +0530

    Time: 0 ms (100%), Space: 8.9 MB (35.14%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -1,44 +1,20 @@
-// class Solution {
-// public:
-//     int arrangeCoins(int n) {
-//         if(n==1) return 1;
-//         int row =1;
-//         int cnt = 0;
-//         int coins = n;
-//         while(row<n){
-//                 coins = coins-row;
-//                 if(coins<0){
-//                     break;
-//                 }else{
-//                     cnt++;
-//                 }
-//             row++;
-//         }
-//         return cnt;
-//     }
-// };
-
 class Solution {
 public:
-    long long sum(long long m){
-        return m*(m+1)/2;
-    }
     int arrangeCoins(int n) {
-        long long m;
-        long long s=1;
-        long long e=n;
-        int index=0;
-        while(s<=e){
-             m=s+(e-s)/2;
-            long long ans=sum(m);
-            if(ans<=n){
-                index=m;
-                s=m+1;
-            }
-            else if(ans>n){
-                e=m-1;
+        long long left = 1, right = n, ans = 0;
+
+        while (left <= right) {
+            long long mid = left + (right - left) / 2;
+            long long coins = mid * (mid + 1) / 2;
+
+            if (coins <= n) {
+                ans = mid;   // mid is possible, try bigger
+                left = mid + 1;
+            } else {
+                right = mid - 1; // too many coins
             }
         }
-        return index;
+
+        return (int)ans;
     }
 };
