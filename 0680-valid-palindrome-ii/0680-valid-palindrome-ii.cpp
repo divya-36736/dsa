2f4eb493cdf1b3b953c182371fbdbe6d5757ba85
divya-36736 <23bec036@nith.ac.in>
2025-09-21 17:48:52 +0530

    Time: 5 ms (45.1%), Space: 26.7 MB (22.45%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool val(string s, int l, int r){
+        while(l<r){
+            if(s[l] != s[r]) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+    bool validPalindrome(string s) {
+        int l = 0;
+        int r = s.size()-1;
+        while(l<r){
+            if(s[l] != s[r]){
+                return val(s, l+1, r) || val(s, l, r-1);
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
