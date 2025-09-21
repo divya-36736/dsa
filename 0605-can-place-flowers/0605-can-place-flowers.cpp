e49d69fcd66c3de21e0bf62687f934075bfa9274
divya-36736 <23bec036@nith.ac.in>
2025-09-21 13:00:28 +0530

    Time: 0 ms (100%), Space: 24 MB (74.23%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,27 +1,42 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        int m = flowerbed.size();
+        // int m = flowerbed.size();
         
-        if (m >= 1 && flowerbed[0] == 0 && (m == 1 || flowerbed[1] != 1)) {
-            flowerbed[0] = 1;
-            n--;
-        }
+        // if (m >= 1 && flowerbed[0] == 0 && (m == 1 || flowerbed[1] != 1)) {
+        //     flowerbed[0] = 1;
+        //     n--;
+        // }
 
-        if (m >= 2 && flowerbed[m-1] == 0 && flowerbed[m-2] != 1) {
-            flowerbed[m-1] = 1;
-            n--;
-        }
+        // if (m >= 2 && flowerbed[m-1] == 0 && flowerbed[m-2] != 1) {
+        //     flowerbed[m-1] = 1;
+        //     n--;
+        // }
 
-        int i = 1;
-        while (i < m-1 && n > 0) {
-            if (flowerbed[i] == 0 && flowerbed[i-1] != 1 && flowerbed[i+1] != 1) {
-                flowerbed[i] = 1;
-                n--;
+        // int i = 1;
+        // while (i < m-1 && n > 0) {
+        //     if (flowerbed[i] == 0 && flowerbed[i-1] != 1 && flowerbed[i+1] != 1) {
+        //         flowerbed[i] = 1;
+        //         n--;
+        //     }
+        //     i++;
+        // }
+
+        // return n <= 0;
+
+
+        int m = flowerbed.size();
+        for (int i = 0; i < m && n > 0; i++) {
+            if (flowerbed[i] == 0) {
+                int prev = (i == 0 ? 0 : flowerbed[i-1]);
+                int next = (i == m-1 ? 0 : flowerbed[i+1]);
+                
+                if (prev == 0 && next == 0) {
+                    flowerbed[i] = 1;
+                    n--;
+                }
             }
-            i++;
         }
-
-        return n <= 0;
+        return n == 0;
     }
 };
