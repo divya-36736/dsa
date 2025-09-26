4a151e77af675e862c4be9b0a0304ae81fab65fe
divya-36736 <23bec036@nith.ac.in>
2025-09-26 08:00:25 +0530

    Time: 0 ms (100%), Space: 8.3 MB (89.19%) - LeetHub

--- 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.cpp
+++ 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.cpp
@@ -4,13 +4,25 @@ public:
         unordered_set<char>st(str.begin(), str.end());
         return st.size() == str.size();  // all chars must be unique
     }
-    
+
     int countGoodSubstrings(string s) {
+        //int n = s.size();
+        // int cnt = 0;
+        // for(int i = 0; i<=n-3; i++){
+        //     string sub = s.substr(i, 3);
+        //     if(isGood(sub)){
+        //         cnt++;
+        //     }
+        // }
+        // return cnt;
+
         int n = s.size();
+        if (n < 3) return 0;
+
         int cnt = 0;
-        for(int i = 0; i<=n-3; i++){
-            string sub = s.substr(i, 3);
-            if(isGood(sub)){
+        for (int i = 0; i <= n - 3; i++) {
+            char a = s[i], b = s[i+1], c = s[i+2];
+            if (a != b && b != c && a != c) {
                 cnt++;
             }
         }
