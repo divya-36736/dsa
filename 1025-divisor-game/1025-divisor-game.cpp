0181374e571735463e4ed5cb24b42a75c143692a
divya-36736 <23bec036@nith.ac.in>
2025-12-18 14:05:39 +0530

    Time: 0 ms (100%), Space: 7.7 MB (59.16%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     bool divisorGame(int n) {
-        //if n is even
+        //if n is even the  alice take 1 and leave odd for bob
+        //if n is odd then alice take 1 and leave even for bob;
+        //thus we can say alice wins if and if n is enen
         if(n%2 == 0){
             return true;
         }
