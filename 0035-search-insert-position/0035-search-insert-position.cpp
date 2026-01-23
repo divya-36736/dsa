ce8bf316e4c3f7d33a23203d2a74493551a3982e
divya-36736 <23bec036@nith.ac.in>
2026-01-23 18:06:51 +0530

    Time: 0 ms (100%), Space: 13.7 MB (41.24%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -4,22 +4,19 @@ public:
         int n = nums.size();
         int st = 0;
         int end = n-1;
-        int ans = n;
+        int ans = 0;
         while(st<=end){
             int mid = st + (end-st)/2;
             if(nums[mid] == target){
-                ans = mid;
-                break;
+                return mid;
             }
             else if(nums[mid] < target){
-                ans = mid + 1;
-                st = mid + 1;
+                ans = mid+1;
+                st = mid+1;
             }
             else{
-                ans = mid;
-                end = mid - 1;
+                end = mid-1;
             }
-            
         }
         return ans;
     }
