42aa82940042c45f8c40876c2da34e8226b865cf
divya-36736 <23bec036@nith.ac.in>
2026-02-01 15:45:44 +0530

    Time: 0 ms (100%), Space: 14.8 MB (25.26%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-         int maxi = 0;
+        
+        int maxi = INT_MIN;
+        for(auto it:trips){
+            maxi = max(maxi, it[2]);
+        }
+        vector<int>diff(maxi, 0);
+        for(auto it : trips){
+            int pass = it[0];
+            int from = it[1];
+            int to = it[2];
 
-         for (auto it : trips){
-             maxi = max(maxi,it[2]);
-         }
-         
-         vector<int>diff(maxi + 1,0);
-         for (auto it : trips){
-              int from = it[1];
-              int to = it[2];
-              int pass = it[0];
-
-              diff[from] += pass;
-              if (to  < maxi ){
-                diff[to ] -= pass;
-              }
-         }
-         if (diff[0] > capacity) return false;
-
-         for (int i = 1 ; i < diff.size() ;i++){
+            diff[from] += pass;
+            if(to < maxi){
+                diff[to] -= pass;
+            }
+        }
+        if(diff[0] > capacity) return false;
+        for(int i =1; i<maxi; i++){
             diff[i] = diff[i-1] + diff[i];
-            if (diff[i] > capacity) return false ;
-            
-         }
-
-         return true ;
+            if(diff[i] > capacity) return false;
+        }
+        return true;
     }
 };
