3f209884d4dd852806b560a9212f6dc71bc80c32
divya-36736 <23bec036@nith.ac.in>
2026-02-02 10:51:45 +0530

    Time: 0 ms (100%), Space: 11.2 MB (6.9%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
         int n=nums.size();
+        //answer is unique numbers in array
         unordered_set<int> s;
         for(int x : nums){
             if(x>0) s.insert(x);
