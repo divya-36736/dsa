14aa5121518864e994057dfdf26a4f9e1392f299
divya-36736 <23bec036@nith.ac.in>
2026-02-02 10:48:59 +0530

    Time: 0 ms (100%), Space: 11 MB (47.11%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -1,28 +1,11 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
-        int high = *max_element(nums.begin(), nums.end());
-        int n = nums.size();
-        int cnt  = 0;
-        while(high){
-            int mini = INT_MAX;
-            for(int i = 0; i<n; i++){
-                if(mini > nums[i] && nums[i] != 0){
-                    mini = nums[i];
-                }
-            }
-            if(mini == INT_MAX){
-                return 0;
-                break;
-            }
-            for(int i = 0; i<n; i++){
-                if(nums[i] != 0){
-                    nums[i] -= mini;
-                }
-            }
-            cnt++;
-            high -= mini;
+        int n=nums.size();
+        unordered_set<int> s;
+        for(int x : nums){
+            if(x>0) s.insert(x);
         }
-        return cnt;
+        return s.size();
     }
 };
