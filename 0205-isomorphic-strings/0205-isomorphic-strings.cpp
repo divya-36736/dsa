dbc55db2c57634d23e3b9e4c629213a5d02773f8
divya-36736 <23bec036@nith.ac.in>
2026-02-03 18:22:37 +0530

    Time: 0 ms (100%), Space: 9.5 MB (27.61%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        int n = s.length();
-        int m = t.length();
-        if(n != m) return false;
-        // for(int i = 0; i<n; i++){
-        //     char c1 =  s[i];
-        //     char c2 = t[i];
-        //     for(int j = 0; j<n; j++){
-        //         if(s[j] == c1 && t[j] != c2){
-        //             return false;
-        //         }
-        //         else if(t[j] == c2 && s[j] != c1){
-        //             return false;
-        //         }
-        //     }
-        // }
-        // return true;
+        if (s.size() != t.size()) return false;
+        int n = s.size();
         unordered_map<char, int> m1, m2;
-        for(int i = 0; i<n; ++i){
-            if(m1.find(s[i]) == m1.end()){
+
+        for (int i = 0; i < n; ++i) {
+            if (m1.find(s[i]) == m1.end()) {
                 m1[s[i]] = i;
             }
-            if(m2.find(t[i]) == m2.end()){
+            if (m2.find(t[i]) == m2.end()) {
                 m2[t[i]] = i;
             }
-            if(m1[s[i]] != m2[t[i]]){
+            if (m1[s[i]] != m2[t[i]]) {
                 return false;
             }
         }
