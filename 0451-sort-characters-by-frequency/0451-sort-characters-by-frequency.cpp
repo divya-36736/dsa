f00f9d07fea335efbfee0d94793a3e974f73d9e3
divya-36736 <23bec036@nith.ac.in>
2026-02-05 10:19:49 +0530

    Time: 3 ms (55.53%), Space: 11.2 MB (48.64%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        vector<pair<char,int>> vec;
+        int n = s.size();
+        vector<pair<char, int>>vec;
 
-        // count using vector<pair>
-        for (char c : s) {
+        for(char c:s){
             bool found = false;
-            for (auto &p : vec) {
-                if (p.first == c) {
+            for(auto &p : vec){
+                if(p.first == c){
                     p.second++;
                     found = true;
                     break;
                 }
             }
-            if (!found) {
+            if(!found){
                 vec.push_back({c, 1});
             }
         }
-
-        // sort by frequency
-        sort(vec.begin(), vec.end(), [](auto &a, auto &b) {
-            return a.second > b.second;
+        //sort
+        sort(vec.begin(), vec.end(), [](auto &a, auto &b){
+            return a.second>b.second;
         });
-
-        // build answer
         string ans;
-        for (auto &p : vec) {
+        for(auto p : vec){
             ans.append(p.second, p.first);
         }
-
         return ans;
     }
 };
