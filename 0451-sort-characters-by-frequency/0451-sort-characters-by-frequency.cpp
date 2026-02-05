ed1912e60bf32fecf0d92d43e1238984471f0417
divya-36736 <23bec036@nith.ac.in>
2026-02-05 10:08:55 +0530

    Time: 2 ms (62.32%), Space: 11.3 MB (38.97%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,48 +1,32 @@
 class Solution {
 public:
-//write static bcz when we call this in sort then then obj 
-//is required but this is not good or correct method then we 
-//here use static if not use static then it is non-static 
-//and non static ko call krne ke liye object ki jarurat hoti hai;
-
-    static bool cmp(pair<char, int>&a, pair<char, int>&b){
-        return a.second>b.second;
-    }
-
     string frequencySort(string s) {
-        //int n = s.size();
-        // unordered_map<char, int> count;
-        // //stores count in map
-        // for(int i = 0; i<n; i++){
-        //     count[s[i]]++;
-        // }
-        // string ans = "";
-        // for(auto it: count){
-        //     int size = it.second;
-        //    for(int i = 0; i<size; i++){
-        //     ans += it.first;
-        //    }
-        // }
-        // return ans;
+        vector<pair<char,int>> vec;
 
-        int n = s.size();
-        unordered_map<char, int> count;
-
-        // stores count in map
-        for (int i = 0; i < n; i++) {
-            count[s[i]]++;
+        // count using vector<pair>
+        for (char c : s) {
+            bool found = false;
+            for (auto &p : vec) {
+                if (p.first == c) {
+                    p.second++;
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                vec.push_back({c, 1});
+            }
         }
 
-        // move (char, freq) pairs to a vector for sorting
-        vector<pair<char, int>> freq(count.begin(), count.end());
+        // sort by frequency
+        sort(vec.begin(), vec.end(), [](auto &a, auto &b) {
+            return a.second > b.second;
+        });
 
-        // sort by frequency in descending order
-        sort(freq.begin(), freq.end(), cmp);
-
-        string ans = "";
-        // append each char freq times
-        for (auto &it : freq) {
-            ans.append(it.second, it.first);
+        // build answer
+        string ans;
+        for (auto &p : vec) {
+            ans.append(p.second, p.first);
         }
 
         return ans;
