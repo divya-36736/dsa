e4b2b4df23c1345778152114d63b6946db5b1e32
divya-36736 <23bec036@nith.ac.in>
2026-02-07 09:03:57 +0530

    Time: 5 ms (58.02%), Space: 10.1 MB (76.17%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
-    int nums(char c){
+    int convert(char c){
+        
         if(c == 'I') return 1;
-        else if(c == 'V') return 5;
         else if(c == 'X') return 10;
+        else if(c == 'V') return 5;
         else if(c == 'L') return 50;
         else if(c == 'C') return 100;
         else if(c == 'D') return 500;
         else return 1000;
-    }
+    };
     int romanToInt(string s) {
-        int n  = s.size();
-        int idx = 0;
         int sum = 0;
-
-        while(idx<(n-1)){
-            if(nums(s[idx])<nums(s[idx+1])){
-                //ydi less hoga toh hum - kr rhe hai
-                sum -= nums(s[idx]);
-            }else{
-                sum += nums(s[idx]); 
+        int n = s.size();
+        for(int i = 0; i<n-1; i++){
+            if(convert(s[i]) < convert(s[i+1])){
+                //-hoga
+                sum -= convert(s[i]);
             }
-            idx++;
+            else{
+                sum += convert(s[i]);
+            }
         }
-        //last n-1 elemets rah rha tha
-        sum += nums(s[n-1]);
+        sum += convert(s[n-1]);
         return sum;
     }
 };
