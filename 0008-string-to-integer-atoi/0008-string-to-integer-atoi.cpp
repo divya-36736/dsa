4341e5507d830b081b1aa7e960a101d20ca2fe3b
divya-36736 <23bec036@nith.ac.in>
2026-02-07 22:01:03 +0530

    Time: 0 ms (100%), Space: 9.2 MB (21.77%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int i = 0;
         int n = s.size();
-
-        // Skip leading spaces
-        while (i < n && s[i] == ' ') {
+        int sign = 1;
+        int i = 0;
+        
+        while(i<n && s[i] == ' '){
             i++;
         }
-
-        // Check sign
-        int sign = 1;
-        if (i < n && (s[i] == '-' || s[i] == '+')) {
+        //check sign
+        if(i<n && (s[i] == '-' || s[i] == '+')){
             sign = (s[i] == '-') ? -1 : 1;
             i++;
         }
-
         long ans = 0;
-        while (i < n && isdigit(s[i])) {
+        while(i<n && isdigit(s[i])){
             ans = ans * 10 + (s[i] - '0');
-            if (sign == 1 && ans > INT_MAX) return INT_MAX;
-            if (sign == -1 && -ans < INT_MIN) return INT_MIN;
+            if(sign == 1 && ans > INT_MAX) return INT_MAX;
+            if(sign == -1 && -ans < INT_MIN) return INT_MIN;
             i++;
         }
-
-        return (int)(sign * ans);
+        return (int)(sign*ans);
     }
 };
