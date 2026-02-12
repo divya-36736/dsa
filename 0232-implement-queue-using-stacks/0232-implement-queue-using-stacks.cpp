ddc767ddb021780aefafe8a076cebc820b135f6f
divya-36736 <23bec036@nith.ac.in>
2026-02-12 13:43:46 +0530

    Time: 0 ms (100%), Space: 9.7 MB (68.57%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -7,15 +7,15 @@ public:
     }
     
     void push(int x) {
-       while(!s1.empty()){
-        s2.push(s1.top());
-        s1.pop();
-       }
-       s1.push(x);
-       while(!s2.empty()){
-        s1.push(s2.top());
-        s2.pop();
-       }
+        while(!s1.empty()){
+            s2.push(s1.top());
+            s1.pop();
+        }
+        s1.push(x);
+        while(!s2.empty()){
+            s1.push(s2.top());
+            s2.pop();
+        }
     }
     
     int pop() {
