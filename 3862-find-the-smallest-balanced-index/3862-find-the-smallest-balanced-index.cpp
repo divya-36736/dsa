3c0eb8cb4572ab2787dfe7cffe99c937a497a47c
divya-36736 <23bec036@nith.ac.in>
2026-03-16 00:18:29 +0530

    Time: 6 ms (80.75%), Space: 192.5 MB (82.96%) - LeetHub

--- 3862-find-the-smallest-balanced-index/3862-find-the-smallest-balanced-index.cpp
+++ 3862-find-the-smallest-balanced-index/3862-find-the-smallest-balanced-index.cpp
@@ -2,29 +2,16 @@ class Solution {
 public:
     int smallestBalancedIndex(vector<int>& nums) {
         int n = nums.size();
-        if(n == 1) return -1;
-        if(n == 2){
-            if(nums[0] == 1) return 1;
-        }
-        vector<long long>leftsum(n);
-        vector<long long>rightPro(n);
-        leftsum[0] = nums[0];
-        for(int i = 1; i<n; i++){
-            leftsum[i] = leftsum[i-1]+nums[i];
-        }
-        rightPro[n-1] = nums[n-1];
-        for(int i = n-2; i>=0; i--){
-            if(rightPro[i+1] > LLONG_MAX / nums[i]){
-                rightPro[i] = LLONG_MAX;
-            } else {
-                rightPro[i] = rightPro[i+1] * nums[i];
-            }
-        }
+        long long lsum = 0;
+        for(int x : nums) lsum += x;
 
-        for(int i = 1; i<n-1; i++){
-            if(leftsum[i-1] == rightPro[i+1]){
-                return i;
-            }
+        long long rpro = 1;
+        for(int i = n-1; i>=0; i--){
+            lsum -= nums[i];
+            if(lsum == rpro) return i;
+            if(rpro > lsum/nums[i]) break;
+
+            rpro *= nums[i];
         }
         return -1;
     }
