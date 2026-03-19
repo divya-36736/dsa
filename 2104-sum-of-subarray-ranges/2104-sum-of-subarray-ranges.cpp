08ef6bdbaa90256d461dbe871dcc4b71ad5166b4
divya-36736 <23bec036@nith.ac.in>
2026-03-19 18:57:26 +0530

    Time: 0 ms (100%), Space: 15.6 MB (59.32%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -2,16 +2,53 @@ class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
         int n = nums.size();
-        long long sum = 0;
+        stack<int>st;
+        //mininum
+        vector<int>leftmin(n), rightmin(n);
+        //pse
         for(int i = 0; i<n; i++){
-            int maxi = INT_MIN;
-            int mini = INT_MAX;
-            for(int j = i; j<n; j++){
-                maxi = max(maxi, nums[j]);
-                mini = min(mini, nums[j]);
-                sum += abs(maxi - mini);
+            while(!st.empty() && nums[st.top()] > nums[i]){
+                st.pop();
             }
+            leftmin[i] = st.empty()?i+1:i-st.top();
+            st.push(i);
         }
-        return sum;
+        while(!st.empty()) st.pop();
+        //nse
+        for(int i = n-1; i>=0; i--){
+            while(!st.empty() && nums[st.top()] >= nums[i]){
+                st.pop();
+            }
+            rightmin[i] = st.empty() ? n-i : st.top()-i;
+            st.push(i);
+        }
+
+        while(!st.empty()) st.pop();
+        vector<int>leftmax(n), rightmax(n);
+        //pge
+        for(int i = 0; i<n; i++){
+            while(!st.empty() && nums[st.top()] < nums[i]){
+                st.pop();
+            }
+            leftmax[i] = st.empty()? i+1 : i-st.top();
+            st.push(i);
+        }
+        while(!st.empty()) st.pop();
+        //nge
+        for(int i = n-1; i>=0; i--){
+            while(!st.empty() && nums[st.top()] <= nums[i]){
+                st.pop();
+            }
+            rightmax[i] = st.empty() ? n-i : st.top() - i;
+            st.push(i);
+        }
+        //contribution 
+        long long maxsum = 0;
+        long long minsum = 0;
+        for(int i = 0; i<n; i++){
+            minsum += (long long)nums[i] * leftmin[i] * rightmin[i];
+            maxsum += (long long)nums[i] * leftmax[i] * rightmax[i];
+        }
+        return maxsum - minsum;
     }
 };
