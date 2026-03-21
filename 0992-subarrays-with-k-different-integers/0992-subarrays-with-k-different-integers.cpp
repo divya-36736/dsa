0d941a4a0bae8bed5a62d18fc3ff40dd41576b8a
divya-36736 <23bec036@nith.ac.in>
2026-03-21 15:30:10 +0530

    Time: 164 ms (8.22%), Space: 67.3 MB (12.16%) - LeetHub

---
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int solve(vector<int>&nums, int k){
+        int n = nums.size();
+        int l = 0;
+        int r = 0;
+        int count = 0;
+        map<int,int>mpp;
+
+        while(r<n){
+            mpp[nums[r]]++;
+            while(mpp.size()>k){
+                mpp[nums[l]]--;
+                if(mpp[nums[l]] == 0) mpp.erase(nums[l]);
+                l++;
+            }
+            count += (r-l+1);
+            r++;
+        }
+        return count;
+    }
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        int n = nums.size();
+        return solve(nums, k) - solve(nums, k-1);
+    }
+};
