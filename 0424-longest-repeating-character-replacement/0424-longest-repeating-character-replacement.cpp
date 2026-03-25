29309ca477402ffe5f6595db722406192f94918d
divya-36736 <23bec036@nith.ac.in>
2026-03-26 01:35:33 +0530

    Time: 39 ms (5.45%), Space: 23.2 MB (5.33%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,26 +1,36 @@
 class Solution {
 public:
-    int characterReplacement(string s, int k) {
+    int changes(string s, int k, char target) {
         int n = s.size();
-        int l = 0;
-        int r = 0;
+        int left = 0;
+        int cnt = 0; // number of changes used
         int maxlen = 0;
-        int maxfreq = 0;
-        unordered_map<int, int>m;
-        while(r<n){
-            m[s[r]]++;
-            maxfreq = max(maxfreq, m[s[r]]);
-            
-            if(r-l+1 - maxfreq > k){
-                m[s[l]]--;
-                if(m[s[l]] == 0){
-                    m.erase(s[l]);
-                }
-                l++;
+
+        for (int right = 0; right < n; right++) {
+            if (s[right] != target) {
+                cnt++; // need to change this
             }
-            maxlen = max(maxlen, r-l+1);
-            r++;
+
+            while (cnt > k) {
+                if (s[left] != target) {
+                    cnt--;
+                }
+                left++;
+            }
+
+            maxlen = max(maxlen, right - left + 1);
         }
         return maxlen;
+    }
+
+    int characterReplacement(string s, int k) {
+        int ans = 0;
+
+        // try making all characters equal to each letter
+        for (char ch = 'A'; ch <= 'Z'; ch++) {
+            ans = max(ans, changes(s, k, ch));
+        }
+
+        return ans;
     }
 };
