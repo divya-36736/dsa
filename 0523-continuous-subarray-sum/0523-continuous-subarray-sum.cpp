29376d3147107d87c95f36058ead6986590abe46
divya-36736 <23bec036@nith.ac.in>
2026-03-27 12:20:35 +0530

    Time: 77 ms (59.73%), Space: 146.3 MB (53.31%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        //if we do the prefix sum and if (prefixsum[j]-prefixsum[i])%k == 0
-        //means prefixsum[j]%k = prefixsum[i]%k means remainder equal this we can done via if we store the remainder in map and if we check that same remainder exist in map then it give true othervise give false' 
+        // if we do the prefix sum and if (prefixsum[j]-prefixsum[i])%k == 0
+        // means prefixsum[j]%k = prefixsum[i]%k means remainder equal this we
+        // can done via if we store the remainder in map and if we check that
+        // same remainder exist in map then it give true othervise give false'
         int n = nums.size();
-        unordered_map<int, int>m;
-        m[0] = -1; //this is bcz if 35%7 = 0 but it is not in maap then it give false , but this is valid then it is
+        unordered_map<int, int> m;
+        m[0] = -1; // this is bcz if 35%7 = 0 but it is not in maap then it give
+                   // false , but this is valid then it is
         int prefix = 0;
-        for(int i =0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             prefix += nums[i];
-            int rem = prefix%k;
-            if(m.find(rem) != m.end()){
-                if (i - m[rem] >= 2)
+            int rem = prefix % k;
+            if (m.find(rem) != m.end()) {
+                if (i - m[rem] >= 2)//len
                     return true;
-            }else{
+            } else {
                 m[rem] = i;
             }
         }
