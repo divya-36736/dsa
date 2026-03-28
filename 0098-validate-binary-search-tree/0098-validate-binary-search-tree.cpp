b03e86702cd1cffd9bae5d0cb58c00913722bb55
divya-36736 <23bec036@nith.ac.in>
2026-03-29 00:01:14 +0530

    Time: 0 ms (100%), Space: 22.2 MB (8.73%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -11,29 +11,18 @@
  */
 class Solution {
 public:
-    // void inorder(TreeNode* root, vector<int>&ans){
-    //     if(root == NULL) return;
-    //     inorder(root->left, ans);
-    //     ans.push_back(root->val);
-    //     inorder(root->right, ans);
-    // }
-
-    bool helper(TreeNode* root, TreeNode* min, TreeNode* max){
-        if(root == NULL) return true;
-        if(min != NULL && root->val <= min->val) return false;
-        if(max != NULL && root->val >= max->val) return false;
-        return helper(root->left, min, root) && helper(root->right, root, max);
+    void inorder(TreeNode* root, vector<int>&res){
+        if(root == NULL) return;
+        inorder(root->left, res);
+        res.push_back(root->val);
+        inorder(root->right, res);
     }
-
     bool isValidBST(TreeNode* root) {
-        // vector<int>ans;
-        // inorder(root, ans);
-        // for(int i = 1; i<ans.size(); i++){
-        //     if(ans[i] <= ans[i-1]) return false;
-        // }
-        // return true;
-
-        
-        return helper(root, NULL, NULL);
+        vector<int>res;
+        inorder(root, res);
+        for(int i = 1; i<res.size(); i++){
+            if(res[i] <= res[i-1]) return false;
+        }
+        return true;
     }
 };
