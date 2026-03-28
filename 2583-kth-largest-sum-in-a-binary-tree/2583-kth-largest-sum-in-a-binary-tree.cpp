34af48ba515a0862e16b70a35f56a5b841ec5eb9
divya-36736 <23bec036@nith.ac.in>
2026-03-28 23:21:39 +0530

    Time: 48 ms (31.2%), Space: 266.9 MB (53.14%) - LeetHub

--- 2583-kth-largest-sum-in-a-binary-tree/2583-kth-largest-sum-in-a-binary-tree.cpp
+++ 2583-kth-largest-sum-in-a-binary-tree/2583-kth-largest-sum-in-a-binary-tree.cpp
@@ -6,34 +6,37 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    vector<long long> level(TreeNode* root){
-        vector<long long>res;
-        if(root == NULL) return res;
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty()){
-            int size = q.size();
-            long long sum = 0;
-            for(int i = 0; i<size; i++){
-                TreeNode* node = q.front();
-                q.pop();
-                sum += node->val;
-                if(node->left != NULL) q.push(node->left);
-                if(node->right != NULL) q.push(node->right);
-            }
-            res.push_back(sum);
-        }
-        return res;
-    }
     long long kthLargestLevelSum(TreeNode* root, int k) {
-        vector<long long>res = level(root);
+        vector<long long> res; 
+        queue<TreeNode*> q;    
+
+        q.push(root); 
+        while (!q.empty()) {
+            int n = q.size();  
+            long long sum = 0; 
+
+            while (n--) {
+                TreeNode* node = q.front();
+                q.pop(); 
+                sum +=
+                    (long long)node->val; 
+
+                if (node->left)
+                    q.push(node->left);
+                if (node->right)
+                    q.push(node->right);
+            }
+            res.push_back(sum); 
+        }
         int n = res.size();
-        if(k > n) return -1;
+        if (k > n)
+            return -1;
         sort(res.begin(), res.end());
         return res[n-k];
     }
