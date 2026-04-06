c10f25c82c4f5b32ea7057915e78109d231e9eec
divya-36736 <23bec036@nith.ac.in>
2026-04-06 11:54:31 +0530

    Time: 0 ms (100%), Space: 14.9 MB (62.84%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,34 +11,26 @@
  */
 class Solution {
 public:
-    vector<vector<int>> levelOrder(TreeNode* root){
-        vector<vector<int>>ans;
-        if(!root) return ans;
+    vector<int> rightSideView(TreeNode* root) {
+        if(root == NULL) return {};
+        vector<int>ans;
         queue<TreeNode*>q;
         q.push(root);
 
         while(!q.empty()){
             int size = q.size();
-            vector<int>level;
-
             for(int i = 0; i<size; i++){
-                TreeNode* top = q.front();
-                level.push_back(top->val);
+                TreeNode* node = q.front();
                 q.pop();
-
-                if(top->left != NULL) q.push(top->left);
-                if (top->right != NULL)  q.push(top->right);
-            } 
-            ans.push_back(level);
+                if(i == size-1) ans.push_back(node->val);
+                if(node->left){
+                    q.push(node->left);
+                }
+                if(node->right){
+                    q.push(node->right);
+                }
+            }
         }
         return ans;
-    }
-    vector<int> rightSideView(TreeNode* root) {
-        vector<int>res;
-        vector<vector<int>>levelTraversal = levelOrder(root);
-        for(auto level: levelTraversal){
-            res.push_back(level.back());
-        }
-        return res;
     }
 };
