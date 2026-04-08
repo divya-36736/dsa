4f83a5c7d2edb61fb56c5b821bcfd6641d6a9942
divya-36736 <23bec036@nith.ac.in>
2026-04-08 19:55:08 +0530

    Time: 183 ms (46.9%), Space: 183.7 MB (30.98%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
-        int n = matches.size();
-        unordered_map<int, int>win;
-        unordered_map<int, int>lose;
-        vector<vector<int>>ans;
+        
+        unordered_map<int,int>mp ;
+        for (auto it : matches){
+            // winner
+            if (!mp.count(it[0]))
+            mp[it[0]] = 0;
+            // loser
+            mp[it[1]]++;
+        }
 
-        for(int i = 0; i<n; i++){
-            win[matches[i][0]]++;
-            lose[matches[i][1]]++;
+        vector<int>win;
+        vector<int>lost;
+        
+
+        for(auto it : mp){
+            if (it.second == 0) win.push_back(it.first);
+            if (it.second == 1) lost.push_back(it.first);
         }
-        vector<int>ans1;
-        for(auto it: win){
-            int winin = it.first;
-            if(lose.find(winin) == lose.end()){
-                ans1.push_back(winin);
-            }
-        }
-        sort(ans1.begin(), ans1.end());
-        ans.push_back(ans1);
-        vector<int>ans2;
-        for(auto it:lose){
-            int losein = it.first;
-            if(it.second == 1){
-                ans2.push_back(losein);
-            }
-        }
-        sort(ans2.begin(), ans2.end());
-        ans.push_back(ans2);
-        return ans;
+        sort(win.begin() ,win.end());
+        sort(lost.begin() ,lost.end());
+        
+        vector<vector<int>>ans ;
+        ans.push_back(win);
+        ans.push_back(lost);
+
+        return ans ;
+
+
     }
 };
