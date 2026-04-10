a9e218604b5ec5fabe12bc992377492b298072ec
divya-36736 <23bec036@nith.ac.in>
2026-04-10 17:12:49 +0530

    Time: 7 ms (36.1%), Space: 27.7 MB (17.84%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,76 +1,39 @@
-// /**
-//  * Definition for a binary tree node.
-//  * struct TreeNode {
-//  *     int val;
-//  *     TreeNode *left;
-//  *     TreeNode *right;
-//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-//  * };
-//  */
-// class Solution {
-// public:
-//     int search(vector<int>inorder, int left, int right, int val){
-//         for(int i = left; i<=right; i++){
-//             if(inorder[i] == val){
-//                 return i;
-//             }
-//         }
-//         return -1;
-//     }
-
-//     TreeNode* helper(vector<int>& preorder, vector<int>& inorder, int &preidx, int left, int right){
-//         if(left > right){
-//             return NULL;
-//         }
-
-//         TreeNode* root = new TreeNode(preorder[preidx]);
-        
-//         int inidx = search(inorder, left, right, preorder[preidx]);
-//         preidx++;
-//         root->left = helper(preorder, inorder, preidx, left, inidx-1); //left subtree
-//         root->right = helper(preorder, inorder, preidx, inidx+1, right);//right subtree
-
-//         return root;
-//     }
-
-//     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-//         int preidx = 0;
-//         return helper(preorder, inorder, preidx, 0, inorder.size()-1);
-       
-//     }
-// };
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    TreeNode* helper(vector<int>& preorder, vector<int>& inorder, int &preidx,
-                     int left, int right, unordered_map<int,int>& mpp) {
-        if (left > right) return NULL;
+    TreeNode*build(vector<int>&preorder, int preSt, int preEnd, vector<int>&inorder, int inst, int inEnd, map<int, int>&m){
+        if(preSt > preEnd || inst>inEnd) return NULL;
 
-        // root node = current preorder element
-        TreeNode* root = new TreeNode(preorder[preidx]);
+        //construct root
+        TreeNode* root = new TreeNode(preorder[preSt]);
+        //root find in inorder
+        int inroot = m[root->val];
+        //now split according numleft
+        int numleft = inroot - inst;
 
-        // find root index in inorder using map
-        int inidx = mpp[preorder[preidx]];
-        preidx++;
-
-        // recursively build left and right
-        root->left = helper(preorder, inorder, preidx, left, inidx - 1, mpp);
-        root->right = helper(preorder, inorder, preidx, inidx + 1, right, mpp);
+        root->left = build(preorder, preSt+1, preSt+numleft, inorder, inst, inroot-1, m);
+        root->right = build(preorder, preSt+numleft+1, preEnd, inorder, inroot+1, inEnd, m);
 
         return root;
     }
-
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        int preidx = 0;
+        map<int, int>m;
+        int n = preorder.size();
 
-        // build map of inorder value -> index
-        unordered_map<int,int> mpp;
-        for (int i = 0; i < inorder.size(); i++) {
-            mpp[inorder[i]] = i;
+        for(int i = 0; i<n; i++){
+            m[inorder[i]] = i;
         }
 
-        return helper(preorder, inorder, preidx, 0, inorder.size() - 1, mpp);
+        return build(preorder, 0, n-1, inorder, 0, n-1, m);
     }
 };
