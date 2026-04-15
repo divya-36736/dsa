7f970ebcfbc18c36b489ce2bc9ac8710c09d18f1
divya-36736 <23bec036@nith.ac.in>
2026-04-15 19:06:47 +0530

    Time: 0 ms (100%), Space: 8.7 MB (17.12%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    int memo(int n, vector<int>&dp){
-        if(n<=2) return n;
-        if(dp[n] != -1) return dp[n];
-        dp[n] = memo(n-1, dp)+memo(n-2, dp);
-        return dp[n];
-    } 
+    
     int climbStairs(int n) {
-        vector<int>dp(n+1, -1);
-        
-        return memo(n, dp);
+        if(n<=2) return n;
+        vector<int>dp(n+1, 0);
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i<=n; i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
