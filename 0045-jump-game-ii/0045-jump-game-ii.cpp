8363ec28d7b3cd887f42fec40e00e9b7adb58cc9
divya-36736 <23bec036@nith.ac.in>
2026-05-20 18:55:52 +0530

    Time: 0 ms (100%), Space: 20.4 MB (83%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
-    int minjump(vector<int>&nums, int pos, vector<int>&dp){
-        int n = nums.size();
-        if(pos >= n-1) return 0;
-
-        if(nums[pos] == 0) return INT_MAX;
-
-        int minstep = INT_MAX;
-
-        if(dp[pos] != -1) return dp[pos];
-
-        for(int jump=1; jump<=nums[pos]; jump++){
-            
-            int subres = minjump(nums, pos+jump, dp);
-
-            if(subres != INT_MAX){
-                minstep = min(minstep, 1+subres);
-            }
-        }
-        return dp[pos] = minstep;
-    }
     int jump(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n, -1);
-        return minjump(nums, 0, dp);
+        int jumps = 0;
+        int l = 0;
+        int r = 0;
+        while(r<n-1){
+            int farpoint  = 0;
+            for(int i = l; i<=r; i++){
+                farpoint = max(farpoint, i+nums[i]);
+            }
+            l= r + 1;
+            r = farpoint;
+            jumps++;
+        }
+        return jumps;
     }
 };
