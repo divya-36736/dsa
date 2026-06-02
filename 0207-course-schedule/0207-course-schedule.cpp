56411ad7d5f7035b269451787a4b8e7fb266179d
divya-36736 <23bec036@nith.ac.in>
2026-06-02 15:52:18 +0530

    Time: 0 ms (100%), Space: 19.2 MB (66.8%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,40 +1,45 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-
+        //adjlist
         vector<vector<int>> adj(numCourses);
-        for (auto edge : prerequisites) {
-            int u = edge[1]; // Source (prerequisite course)
-            int v = edge[0]; // Destination (dependent course)
-            adj[u].push_back(v);
+        for(auto &edge: prerequisites){
+            int u = edge[0];
+            int v = edge[1];
+            adj[v].push_back(u);
         }
 
-        vector<int>ind(numCourses, 0);
+        
+        vector<int>indegree(numCourses, 0);
         for(int i = 0; i<numCourses; i++){
-            for(auto it: adj[i]){
-                ind[it]++;
+            for(auto &it: adj[i]){
+                indegree[it]++;
             }
         }
 
+        
         queue<int>q;
+
         for(int i = 0; i<numCourses; i++){
-            if(ind[i] == 0){
-                q.push(i);
+            if(indegree[i] == 0) q.push(i);
             }
-        }
+        
+
         int cnt = 0;
+
         while(!q.empty()){
-            int node = q.front();
+            int it = q.front();
             q.pop();
             cnt++;
-            for(auto it: adj[node]){
-                ind[it]--;
-                if(ind[it] == 0){
-                    q.push(it);
+            for(auto &nbr: adj[it]){
+                indegree[nbr]--;
+                if(indegree[nbr] == 0){
+                    q.push(nbr);
                 }
             }
         }
-        if(cnt == numCourses) return true;
-        return false;
+
+        return cnt == numCourses;
+
     }
 };
