12894608552dc96e54e63f1f158370c5e0323c9d
divya-36736 <23bec036@nith.ac.in>
2026-06-02 10:42:54 +0530

    Time: 7 ms (6.6%), Space: 11.1 MB (21.4%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,46 +1,24 @@
 class Solution {
 public:
-    void subsetH(int i, vector<vector<int>>&ans, vector<int>&ds, vector<int>& nums){  
-        //brute force approach
-        // if(i == nums.size()){
-        //     ans.insert(ds);
-        //     return;
-        // }
-
-
-        // //take
-        // ds.push_back(nums[i]);
-        // subsetH(i+1, ans, ds, nums);
-        // //not take
-        // ds.pop_back();
-        // subsetH(i+1, ans, ds, nums);
-
-
-        //optimal approach
-        ans.push_back(ds);
-        for(int j = i; j<nums.size(); j++){
-            if(j != i && nums[j] == nums[j-1]) continue; //dublicate skip
-            ds.push_back(nums[j]);
-            subsetH(j+1, ans, ds, nums);
-            ds.pop_back();
-        }
-       
-    }
-
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        // sort(nums.begin(), nums.end());
-        // set<vector<int>>ans;
-        // vector<vector<int>>res;
-        // vector<int>ds;
-        // subsetH(0, ans,ds, nums);
-        // for(auto it = ans.begin(); it != ans.end(); it++){
-        //     res.push_back(*it);
-        // }
-        // return res;
         sort(nums.begin(), nums.end());
-        vector<vector<int>>ans;
-        vector<int>ds;
-        subsetH(0, ans, ds, nums);
-        return ans;
+
+        int n = nums.size();
+        int subsets = 1 << n;
+
+        set<vector<int>> st;
+
+        for(int mask = 0; mask < subsets; mask++) {
+            vector<int> res;
+
+            for(int i = 0; i < n; i++) {
+                if(mask & (1 << i))
+                    res.push_back(nums[i]);
+            }
+
+            st.insert(res);
+        }
+
+        return vector<vector<int>>(st.begin(), st.end());
     }
 };
