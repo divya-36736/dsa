a5f7c5ff21bce30c84512356d7f876934427f3f8
divya-36736 <23bec036@nith.ac.in>
2026-06-07 01:58:27 +0530

    Time: 20 ms (27.99%), Space: 20.6 MB (12.48%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -4,52 +4,68 @@ public:
                     vector<vector<int>>& edges,
                     int distanceThreshold) {
 
-        vector<vector<int>> mat(n, vector<int>(n, 1e9));
-
-        for(int i=0;i<n;i++)
-            mat[i][i] = 0;
+        vector<vector<pair<int,int>>> adj(n);
 
         for(auto &e : edges){
             int u = e[0];
             int v = e[1];
             int wt = e[2];
 
-            mat[u][v] = wt;
-            mat[v][u] = wt;
-        }
-
-        // Floyd Warshall
-        for(int k=0;k<n;k++){
-            for(int i=0;i<n;i++){
-                for(int j=0;j<n;j++){
-
-                    if(mat[i][k] == 1e9 ||
-                       mat[k][j] == 1e9)
-                        continue;
-
-                    mat[i][j] =
-                        min(mat[i][j],
-                            mat[i][k] + mat[k][j]);
-                }
-            }
+            adj[u].push_back({v, wt});
+            adj[v].push_back({u, wt});
         }
 
         int cityNo = -1;
-        int cntcity = INT_MAX;
+        int minCnt = INT_MAX;
 
-        for(int city=0; city<n; city++){
+        for(int src = 0; src < n; src++){
+
+            vector<int> dist(n, 1e9);
+
+            priority_queue<
+                pair<int,int>,
+                vector<pair<int,int>>,
+                greater<pair<int,int>>
+            > pq;
+
+            dist[src] = 0;
+            pq.push({0, src});
+
+            while(!pq.empty()){
+
+                auto [dis, node] = pq.top();
+                pq.pop();
+
+                if(dis > dist[node]) continue;
+
+                for(auto &nbr : adj[node]){
+
+                    int adjNode = nbr.first;
+                    int wt = nbr.second;
+
+                    if(dis + wt < dist[adjNode]){
+
+                        dist[adjNode] = dis + wt;
+
+                        pq.push({
+                            dist[adjNode],
+                            adjNode
+                        });
+                    }
+                }
+            }
 
             int cnt = 0;
 
-            for(int adj=0; adj<n; adj++){
+            for(int i = 0; i < n; i++){
 
-                if(mat[city][adj] <= distanceThreshold)
+                if(dist[i] <= distanceThreshold)
                     cnt++;
             }
 
-            if(cnt <= cntcity){
-                cntcity = cnt;
-                cityNo = city;
+            if(cnt <= minCnt){
+                minCnt = cnt;
+                cityNo = src;
             }
         }
 
