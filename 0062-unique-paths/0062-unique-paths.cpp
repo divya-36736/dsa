fd23c2e911edf4cd4f9fccca86d6f47a06b1ed8f
divya-36736 <23bec036@nith.ac.in>
2026-06-18 02:13:15 +0530

    Time: 3 ms (9.65%), Space: 9.5 MB (28.91%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -13,18 +13,23 @@ public:
     int uniquePaths(int m, int n) {
         vector<vector<int>>dp(m, vector<int>(n, 0));
         //return count(m-1, n-1, m, n, dp);
+        vector<int>prev(n, 0);
+        prev[0] = 1;
+
         for(int i = 0; i<m; i++){
+            vector<int>temp(n, 0);
             for(int j = 0; j<n; j++){
                 if(i == 0 && j == 0){
-                    dp[0][0] = 1;
+                    temp[j] = 1;
                     continue;
                 }
                 int up = 0, left = 0;
-                if(i>0) up = dp[i-1][j];
-                if(j>0) left = dp[i][j-1];
-                dp[i][j] = up+left;
+                if(i>0) up = prev[j];
+                if(j>0) left = temp[j-1];
+                temp[j] = up+left;
             }
+            prev = temp;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 };
