9a1707d94de8041e0d2ff1b87e145febf2cfd5e0
divya-36736 <23bec036@nith.ac.in>
2026-06-18 23:49:24 +0530

    Time: 0 ms (100%), Space: 13.3 MB (41.99%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,42 +1,31 @@
-// class Solution {
-// public:
-//     int n;
-
-//     int dfs(int i, int j, vector<vector<int>>& triangle, vector<vector<int>>& dp) {
-//         // base case: last row
-//         if (i == n - 1) return triangle[i][j];
-
-//         // return if already computed
-//         if (dp[i][j] != -1) return dp[i][j];
-
-//         // move down or diagonal right
-//         int down = triangle[i][j] + dfs(i + 1, j, triangle, dp);
-//         int diag = triangle[i][j] + dfs(i + 1, j + 1, triangle, dp);
-
-//         return dp[i][j] = min(down, diag);
-//     }
-
-//     int minimumTotal(vector<vector<int>>& triangle) {
-//         n = triangle.size();
-//         // initialize memoization table
-//         vector<vector<int>> dp(n, vector<int>(n, -1));
-//         return dfs(0, 0, triangle, dp);
-//     }
-// };
-
 class Solution {
 public:
+    int minsum(int i, int j, int n, vector<vector<int>>&triangle, vector<vector<int>>&dp){
+        if(i>=n || j<0 || j>i) return 1e9;
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(i == n-1) return triangle[i][j];
+
+        int down = minsum(i+1, j, n, triangle, dp);
+        int right = minsum(i+1, j+1, n, triangle, dp);
+
+        return dp[i][j] = triangle[i][j] + min(down, right);
+    }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<int> dp = triangle[n-1]; // start with last row
-
-        // bottom-up
-        for(int i = n-2; i >= 0; i--){
-            for(int j = 0; j <= i; j++){
-                dp[j] = triangle[i][j] + min(dp[j], dp[j+1]);
-            }
+        vector<vector<int>>dp(n, vector<int>(n, 0));
+        //return minsum(0, 0, n, triangle, dp);
+        for(int i = 0; i<triangle[n-1].size(); i++){
+            dp[n-1][i] = triangle[n-1][i];
         }
 
-        return dp[0];
+        for(int i = n-2; i>=0; i--){
+            for(int j = 0; j<triangle[i].size(); j++){
+                int down = dp[i+1][j];
+                int right = dp[i+1][j+1];
+                dp[i][j] = triangle[i][j] + min(down, right);
+            }
+        }
+        return dp[0][0];
     }
 };
