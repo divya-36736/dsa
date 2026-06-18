853d75f7586e0bc5c224acd9859b91f47c8ea489
divya-36736 <23bec036@nith.ac.in>
2026-06-18 22:42:58 +0530

    Time: 0 ms (100%), Space: 16.2 MB (69.61%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,74 +1,20 @@
 class Solution {
 public:
-    int sum(int i, int j, vector<vector<int>>& grid, vector<vector<int>>&dp){
-        //base acse
-        if(i == 0 && j==0){
-            return grid[0][0];
-        }
-        //out of bound
+    int help(int i, int j, int n, int m, vector<vector<int>>&grid, vector<vector<int>>&dp){
+        if(i == 0 && j == 0) return grid[i][j];
         if(i<0 || j<0) return 1e9;
-        //already visited
         if(dp[i][j] != -1) return dp[i][j];
-        //if not visisted the we compute it
-        int up  = sum(i-1, j, grid, dp);
-        int left = sum(i, j-1, grid, dp);
         
+        int up = help(i-1, j, n, m, grid, dp);
+        int left = help(i, j-1, n, m, grid, dp);
+
         return dp[i][j] = grid[i][j] + min(up, left);
     }
     int minPathSum(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        // vector<vector<int>>dp(n, vector<int>(m, -1));
-        // return sum(n-1, m-1, grid, dp);
+       int n = grid.size();
+       int m = grid[0].size();
 
-        //tabulation
-        //1st dp 2d vector
-        
-        // vector<vector<int>>dp(n, vector<int>(m, 0));
-        // for(int i = 0; i<n; i++){
-        //     for(int j = 0; j<m; j++){
-        //         //base case 
-        //         if(i == 0 && j==0){
-        //            dp[i][j] =  grid[i][j];
-        //            continue;
-        //         }
-        //         //now check in up and left
-        //         int up = grid[i][j];
-        //         if(i>0) up += dp[i-1][j];
-        //         else up += 1e9;
-        //         //now in left
-        //         int left = grid[i][j];
-        //         if(j>0) left += dp[i][j-1];
-        //         else left += 1e9; 
-
-        //         dp[i][j] = min(up, left);
-        //     }
-        // }
-        // return dp[n-1][m-1];
-
-        //space optimized
-        vector<int>prev(m, 0); //represent prev row
-        for(int i = 0; i<n; i++){
-            vector<int>temp(m, 0); //store curr row
-            for(int j = 0; j<m; j++){
-                //base case 
-                if(i == 0 && j==0){
-                   temp[j] =  grid[i][j];
-                   continue;
-                }
-                //now check in up and left
-                int up = grid[i][j];
-                if(i>0) up += prev[j];
-                else up += 1e9;
-                //now in left
-                int left = grid[i][j];
-                if(j>0) left += temp[j-1];
-                else left += 1e9; 
-
-                temp[j] = min(up, left);
-            }
-            prev = temp; //update prev after finishing the row
-        }
-        return prev[m-1];
+       vector<vector<int>>dp(n, vector<int>(m, -1));
+       return help(n-1, m-1, n, m, grid, dp); 
     }
 };
