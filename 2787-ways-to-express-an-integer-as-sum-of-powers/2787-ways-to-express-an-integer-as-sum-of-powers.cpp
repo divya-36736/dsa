3c836a71ddba88bdc02d25c265123edaaec7ce9a
divya-36736 <23bec036@nith.ac.in>
2026-06-20 01:15:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
-    static const int MOD = 1e9 + 7;
-    vector<int>powers;
-    int solve(int i, int rem, vector<vector<int>>&dp){
-        //base case
-        if(rem == 0) return 1;
-        if(rem < 0 || i == powers.size()) return 0;
-        if(dp[i][rem] != -1) return dp[i][rem];
-        //take 
-        int take = solve(i+1, rem - powers[i], dp);
-        int notTake = solve(i+1, rem, dp);
+    int const mod = 1e9+7;
+    
+    int solve(int i, int n, vector<int>&power, vector<vector<int>>&dp){
+        if(n == 0) return 1;
+        if(i >= power.size()) return 0;
 
-        return dp[i][rem] = (take+notTake)%MOD;
-        
+        if(dp[i][n] != -1) return dp[i][n];
+
+        //take
+        int take = 0;
+        if(power[i]<=n){
+            take = solve(i+1, n-power[i], power, dp);
+        }
+        //nottake
+        int nottake = solve(i+1, n, power, dp);
+
+        return dp[i][n] = take+nottake;
     }
     int numberOfWays(int n, int x) {
-        for (int i = 1; ; i++) {
+        vector<int>power;
+        for(int i = 1; ; i++){
             long long val = pow(i, x);
-            if (val > n) break;
-            powers.push_back(val);
+            if(val > n) break;
+            power.push_back(val);
         }
-        //like we find a vector [1, 4, 9];
-        //in this we do take not take
-        //show tle then we take dp
-        vector<vector<int>>dp(powers.size(), vector<int>(n+1, -1));
-       return solve(0, n, dp);
+        int m = power.size();
+        vector<vector<int>>dp(m, vector<int>(n+1, -1));
+        return solve(0, n, power, dp);
     }
 };
