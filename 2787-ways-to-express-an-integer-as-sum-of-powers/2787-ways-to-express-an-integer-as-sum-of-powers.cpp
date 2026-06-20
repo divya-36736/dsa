d96b99ac94e6b397017777d0b95b31d1eda863b3
divya-36736 <23bec036@nith.ac.in>
2026-06-20 09:57:43 +0530

    Time: 105 ms (64.27%), Space: 72.1 MB (52.08%) - LeetHub

--- 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -26,7 +26,28 @@ public:
             power.push_back(val);
         }
         int m = power.size();
-        vector<vector<int>>dp(m, vector<int>(n+1, -1));
-        return solve(0, n, power, dp);
+        vector<vector<int>>dp(m, vector<int>(n+1, 0));
+        //return solve(0, n, power, dp);
+        //base case
+        for(int i = 0; i<m; i++){
+            dp[i][0] = 1;
+        }
+        if(power[0] <= n){
+            dp[0][power[0]] = 1;
+        }
+        
+        for(int i = 1; i<m; i++){
+            for(int j = 1; j<=n; j++){
+                int take = 0;
+                if(power[i]<=j){
+                    take = dp[i-1][j-power[i]];
+                }
+                //nottake
+                int nottake = dp[i-1][j];
+
+                dp[i][j] = (take + nottake)%mod;
+            }
+        }
+        return dp[m-1][n];
     }
 };
