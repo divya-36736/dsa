50e49b627bb3bf52c89b8de89f1b55d78f2e1cad
divya-36736 <23bec036@nith.ac.in>
2026-06-21 12:46:34 +0530

    Time: 44 ms (24.8%), Space: 61.3 MB (13.68%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,24 +1,32 @@
+
 class Solution {
 public:
-    int ways(int i, int amount, vector<int>& coins, vector<vector<int>>&dp){
-        int n = coins.size();
-        if(i == 0){
-            return amount % coins[i] == 0;
-        }
-        if(dp[i][amount] != -1) return dp[i][amount];
-        //nottkae
-        int nottake = 0+ways(i-1, amount, coins, dp);
-        //take
-        int take = 0;
-        if(coins[i]<=amount){
-            take =  ways(i, amount-coins[i], coins, dp);
-        }
-        return dp[i][amount] = take+nottake;
-    }
-
     int change(int amount, vector<int>& coins) {
+
         int n = coins.size();
-        vector<vector<int>>dp(n, vector<int>(amount+1, -1));
-        return ways(n-1, amount, coins, dp);
+
+        vector<vector<unsigned long long>> dp(
+            n, vector<unsigned long long>(amount + 1, 0));
+
+        for (int j = 0; j <= amount; j++) {
+            if (j % coins[0] == 0)
+                dp[0][j] = 1;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= amount; j++) {
+
+                unsigned long long take = 0;
+
+                if (coins[i] <= j)
+                    take = dp[i][j - coins[i]];
+
+                unsigned long long nottake = dp[i - 1][j];
+
+                dp[i][j] = take + nottake;
+            }
+        }
+
+        return (int)dp[n - 1][amount];
     }
 };
