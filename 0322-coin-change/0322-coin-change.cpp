3cc21b5f2c42192f3c833de8a72b7ee118497d2b
divya-36736 <23bec036@nith.ac.in>
2026-06-21 12:15:25 +0530

    Time: 49 ms (26.21%), Space: 48.8 MB (41.51%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,89 +1,51 @@
 class Solution {
 public:
-    int minimum(int i, int amount, vector<int>& coins, vector<vector<int>>&dp){
-        int n = coins.size();
-        if(i == 0){
-            if(amount % coins[i] == 0){
-                return amount/coins[i];
-            }
-            else return 1e9;
+    int mini(int i, int amount, vector<int>& coins) {
+        if (i == 0) {
+            if (amount % coins[i] == 0)
+                return amount / coins[i];
+            return 1e9;
         }
-        if(dp[i][amount] != -1) return dp[i][amount];
-        //nottkae
-        int nottake = 0+minimum(i-1, amount, coins, dp);
-        //take
-        int take = INT_MAX;
-        if(coins[i]<=amount){
-            take = 1+ minimum(i, amount-coins[i], coins, dp);
+        // take
+        int take = 1e9;
+        if (coins[i] <= amount) {
+            take = 1 + mini(i, amount - coins[i], coins);
         }
-        return dp[i][amount] = min(take, nottake);
+        // nottake
+        int nottake = mini(i - 1, amount, coins);
+
+        return min(take, nottake);
     }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        // vector<vector<int>>dp(n, vector<int>(amount+1, -1));
-        // int ans = minimum(n-1, amount, coins, dp);
-        // if(ans>= 1e9) return -1;
+
+        // int ans = mini(n - 1, amount, coins);
+        // if (ans == 1e9)
+        //     return -1;
         // return ans;
 
-        //tabulation
-        //make vector same size
-        //vector<vector<int>>dp(n, vector<int>(amount+1, 0));
-        //base case
-        //target ki kisi bhi value ke liye 
-        //if (target%coins[0] == 0) hai toh 
-        //dp[0][i] = i / arr[0]; else dp[0][i] = 1e9
-        // for(int i = 0; i<=amount; i++){
-        //     if(i%coins[0] == 0){
-        //         dp[0][i] = i/coins[0];
-        //     }
-        //     else dp[0][i] = 1e9;
-        // }
-        // //for loop
-        // for(int i = 1; i<n; i++){
-        //     for(int target = 0; target<=amount; target++){
-        //         int nottake = 0+dp[i-1][target];
-        //         //take
-        //         int take = 1e9;
-        //         if(coins[i]<=target){
-        //         take = 1+ dp[i][target-coins[i]];
-        //         } 
-        //         dp[i][target] = min(take, nottake);
-        //     }
-            
-        // }
-        // int ans = dp[n-1][amount];
-        //     if(ans == 1e9){
-        //         return -1;
-        //     }
-        //     return ans;
+        vector<vector<int>> dp(n, vector<int>(amount + 1, 0));
+        for (int j = 0; j <= amount; j++) {
+            if (j % coins[0] == 0)
+                dp[0][j] = j / coins[0];
+            else
+                dp[0][j] = 1e9;
+        }
 
-        //space optimized
-        vector<int>prev(amount+1, 0);//represent the  prev row
-        for(int i = 0; i<=amount; i++){
-            if(i%coins[0] == 0){
-                prev[i] = i/coins[0];
-            }
-            else prev[i] = 1e9;
-        }
-        //for loop
-        for(int i = 1; i<n; i++){
-            vector<int>curr(amount+1,0); //represent the curr row
-            for(int target = 0; target<=amount; target++){
-                int nottake = 0+prev[target];
-                //take
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= amount; j++) {
                 int take = 1e9;
-                if(coins[i]<=target){
-                take = 1+ curr[target-coins[i]];
-                } 
-                curr[target] = min(take, nottake);
+                if (coins[i] <= j) {
+                    take = 1 + dp[i][j - coins[i]];
+                }
+                // nottake
+                int nottake = dp[i - 1][j];
+
+                dp[i][j] = min(take, nottake);
             }
-            prev = curr;
-            
         }
-        int ans = prev[amount];
-            if(ans == 1e9){
-                return -1;
-            }
-            return ans;
+       int ans = dp[n-1][amount];
+       if(ans == 1e9) return -1;
+       else return ans;
     }
 };
