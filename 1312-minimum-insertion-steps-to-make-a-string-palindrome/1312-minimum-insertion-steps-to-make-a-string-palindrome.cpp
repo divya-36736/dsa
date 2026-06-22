389a1e17d78b1778787bc18fc5a010b9703b94a9
divya-36736 <23bec036@nith.ac.in>
2026-06-22 10:36:48 +0530

    Time: 31 ms (63.95%), Space: 31.7 MB (15.02%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -14,8 +14,19 @@ public:
     }
     int lcs(string &s, string &t){
         int n =s.size();
-        vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
-        return longgesplainsub(n, n, s, t, dp);
+        vector<vector<int>>dp(n+1, vector<int>(n+1, 0));
+        //return longgesplainsub(n, n, s, t, dp);
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=n; j++){
+                if(s[i-1] == t[j-1]){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][n];
     }
     int minInsertions(string s) {
         int n =s.size();
