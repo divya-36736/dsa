0e2bb1016d13ef2e6dde5f4306f338ba524c0098
divya-36736 <23bec036@nith.ac.in>
2026-06-23 19:47:03 +0530

    Time: 99 ms (58.83%), Space: 103.3 MB (43.6%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,25 +1,37 @@
 class Solution {
 public:
-    int solve(int i, int hold,int fee, vector<int>&prices, vector<vector<int>>&dp){
+    int solve(int i, int hold,int fee, vector<int>&prices){
         int n = prices.size();
         if(i == n)return 0;
         //
-        if(dp[i][hold] != -1)return dp[i][hold];
-
         if(hold==0){
             //buy skip
-            int buy = -prices[i]+solve(i+1, 1,fee, prices, dp);
-            int skip = solve(i+1, 0,fee, prices, dp);
-            return dp[i][hold] = max(buy, skip);
+            int buy = -prices[i]+solve(i+1, 1,fee, prices);
+            int skip = solve(i+1, 0,fee, prices);
+            return max(buy, skip);
         }else{
-            int sell = prices[i]+solve(i+1, 0,fee, prices, dp);
-            int skip = solve(i+1, 1,fee, prices, dp);
-            return dp[i][hold] = max(sell-fee, skip);
+            int sell = prices[i]+solve(i+1, 0,fee, prices);
+            int skip = solve(i+1, 1,fee, prices);
+            return max(sell-fee, skip);
         }
     }
     int maxProfit(vector<int>& prices, int fee) {
         int n = prices.size();
-        vector<vector<int>>dp(n, vector<int>(2,-1));
-        return solve(0, 0, fee, prices, dp);
+        //return solve(0, 0, fee, prices);
+        vector<vector<int>>dp(n+1, vector<int>(2, 0));
+        for(int i = n-1; i>=0; i--){
+            for(int j = 0; j<=1; j++){
+                if(j == 0){
+                    int buy = -prices[i]+dp[i+1][1];
+                    int skip = dp[i+1][0];
+                    dp[i][j] = max(buy, skip);
+                }else{
+                    int sell = prices[i]+dp[i+1][0];
+                    int skip = dp[i+1][1];
+                    dp[i][j] = max(sell-fee, skip);
+                }
+            }
+        }
+        return dp[0][0];
     }
 };
