263a828448cd7c9804e769ff5cf52c11d812e5bc
divya-36736 <23bec036@nith.ac.in>
2026-06-23 19:07:25 +0530

    Time: 414 ms (36.7%), Space: 211.5 MB (44.18%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,24 +1,45 @@
 class Solution {
 public:
-    int solve(int i, int hold, int trans, vector<int>&prices, vector<vector<vector<int>>>&dp){
-        if(i == prices.size()) return 0;
-        if(trans == 0) return 0;
+    int solve(int i, int hold, int trans, vector<int>& prices,
+              vector<vector<vector<int>>>& dp) {
+        if (i == prices.size())
+            return 0;
+        if (trans == 0)
+            return 0;
 
-        if(dp[i][hold][trans] != -1) return dp[i][hold][trans];
+        if (dp[i][hold][trans] != -1)
+            return dp[i][hold][trans];
 
-        if(hold == 0){
-            int buy = -prices[i]+solve(i+1, 1, trans, prices, dp);
-            int skip = solve(i+1, 0, trans, prices, dp);
+        if (hold == 0) {
+            int buy = -prices[i] + solve(i + 1, 1, trans, prices, dp);
+            int skip = solve(i + 1, 0, trans, prices, dp);
             return dp[i][hold][trans] = max(buy, skip);
-        }else{
-            int sell = prices[i]+solve(i+1, 0, trans-1,prices,  dp);
-            int skip = solve(i+1, 1, trans, prices, dp);
+        } else {
+            int sell = prices[i] + solve(i + 1, 0, trans - 1, prices, dp);
+            int skip = solve(i + 1, 1, trans, prices, dp);
             return dp[i][hold][trans] = max(sell, skip);
         }
     }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>>dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
-        return solve(0, 0, 2, prices, dp);
+        vector<vector<vector<int>>> dp(
+            n+1, vector<vector<int>>(2, vector<int>(3, 0)));
+        // return solve(0, 0, 2, prices, dp);
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j <= 1; j++) {
+                for (int k = 1; k <= 2; k++) {
+                    if (j == 0) {
+                        int buy = -prices[i] + dp[i + 1][1][k];
+                        int skip = dp[i + 1][0][k];
+                        dp[i][j][k] = max(buy, skip);
+                    } else {
+                        int sell = prices[i] + dp[i + 1][0][k-1];
+                        int skip = dp[i + 1][1][k];
+                        dp[i][j][k] = max(sell, skip);
+                    }
+                }
+            }
+        }
+        return dp[0][0][2];
     }
 };
