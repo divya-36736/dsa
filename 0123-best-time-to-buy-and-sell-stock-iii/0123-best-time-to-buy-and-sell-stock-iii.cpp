aecb98b822fd1f2300b5f3ed3273fe86450db59d
divya-36736 <23bec036@nith.ac.in>
2026-06-23 18:56:59 +0530

    Time: 461 ms (17.15%), Space: 225.5 MB (12%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    int solve(int i, int hold, int trans, vector<int>& prices,vector<vector<vector<int>>>&dp) {
-        int n = prices.size();
-        if (i == n || trans == 0) return 0;
+    int solve(int i, int hold, int trans, vector<int>&prices, vector<vector<vector<int>>>&dp){
+        if(i == prices.size()) return 0;
+        if(trans == 0) return 0;
 
         if(dp[i][hold][trans] != -1) return dp[i][hold][trans];
 
-        if (hold == 0) { 
-            int buy = -prices[i] + solve(i+1, 1, trans, prices, dp);
-            int skip = solve(i+1, 0, trans, prices, dp);  // fix here
+        if(hold == 0){
+            int buy = -prices[i]+solve(i+1, 1, trans, prices, dp);
+            int skip = solve(i+1, 0, trans, prices, dp);
             return dp[i][hold][trans] = max(buy, skip);
-        } else {
-            int sell = prices[i] + solve(i+1, 0, trans-1, prices, dp);
+        }else{
+            int sell = prices[i]+solve(i+1, 0, trans-1,prices,  dp);
             int skip = solve(i+1, 1, trans, prices, dp);
             return dp[i][hold][trans] = max(sell, skip);
         }
     }
-
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>>dp(n, vector<vector<int>>(2, vector<int>(3,-1)));
+        vector<vector<vector<int>>>dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
         return solve(0, 0, 2, prices, dp);
     }
 };
