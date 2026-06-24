0242580ba117afbfe38e8bed5e8332914f4530fd
divya-36736 <23bec036@nith.ac.in>
2026-06-24 16:21:05 +0530

    Time: 3 ms (62.08%), Space: 18.4 MB (80.82%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -1,37 +1,42 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class compare{
-    public:
-        bool operator()(ListNode* a, ListNode* b){
-            return a->val > b->val;
-        }
+class Compare {
+public:
+    bool operator()(ListNode* a, ListNode* b) {
+        return a->val > b->val;
+    }
 };
+
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<ListNode*, vector<ListNode*>, compare>p;
-        for(auto node: lists){
-            if(node) p.push(node);
-        }
-        ListNode* root = new ListNode(0);
-        ListNode* tail = root;
-        ListNode* temp;
 
-        while(!p.empty()){
-            temp = p.top();
-            p.pop();
-            tail->next = temp;
-            tail = tail ->next;
-            if(temp->next) p.push(temp->next);
+        priority_queue<
+            ListNode*,
+            vector<ListNode*>,
+            Compare
+        > pq;
+
+        for(auto list : lists) {
+            if(list != NULL) {
+                pq.push(list);
+            }
         }
-        return root->next;
+
+        ListNode* dummy = new ListNode(0);
+        ListNode* tail = dummy;
+
+        while(!pq.empty()) {
+
+            ListNode* smallest = pq.top();
+            pq.pop();
+
+            tail->next = smallest;
+            tail = tail->next;
+
+            if(smallest->next != NULL) {
+                pq.push(smallest->next);
+            }
+        }
+
+        return dummy->next;
     }
 };
