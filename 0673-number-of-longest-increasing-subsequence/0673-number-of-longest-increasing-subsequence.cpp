e13b718790b98935a556aac7e74a7ddda8ba188f
divya-36736 <23bec036@nith.ac.in>
2026-06-25 17:44:47 +0530

    Time: 31 ms (52.5%), Space: 17.4 MB (65.3%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
@@ -5,24 +5,25 @@ public:
         vector<int>dp(n, 1);
         vector<int>cnt(n, 1);
         int maxi = 1;
+
         for(int i = 0; i<n; i++){
             for(int j = 0; j<i; j++){
-                if(nums[j] < nums[i] && dp[j]+1 > dp[i]){
-                    dp[i] = dp[j]+1;
+                if(nums[i]>nums[j] && 1+dp[j] > dp[i]){
+                    dp[i] = 1+dp[j];
                     cnt[i] = cnt[j];
                 }
-                else if(nums[j] < nums[i] && dp[j]+1 == dp[i]){
+                else if(nums[i] > nums[j] && 1+dp[j] == dp[i]){
                     cnt[i] += cnt[j];
                 }
             }
             maxi = max(maxi, dp[i]);
         }
-        int cntLis = 0;
+        int cntlis = 0;
         for(int i = 0; i<n; i++){
             if(dp[i] == maxi){
-                cntLis += cnt[i];
+                cntlis += cnt[i];
             }
         }
-        return cntLis;
+        return cntlis;
     }
 };
