feff618addac63b37d75c7ad0fd0c77ca534b1f7
divya-36736 <23bec036@nith.ac.in>
2026-06-27 22:32:06 +0530

    Time: 4 ms (50%), Space: 19.8 MB (66.05%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    static bool compare(vector<int>&a, vector<int>&b){
-        return a[1]>b[1];
-    }
     int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
         int n = boxTypes.size();
-        int size = truckSize;
-        //we do soring to considering the unit/box bcz we need to maximum 
-        //units
-        sort(boxTypes.begin(), boxTypes.end(), compare);
-        int maxunits = 0;
-        for(int i = 0; i<n; i++){
-            if(size >= boxTypes[i][0]){
-                size = size - boxTypes[i][0];
-                maxunits += (boxTypes[i][0] * boxTypes[i][1]);
+        sort(boxTypes.begin(), boxTypes.end(), [](vector<int> &a, vector<int> &b) {
+        return a[1] > b[1]; 
+        });
+        int i = 0;
+        int t = truckSize;
+        int ans = 0;
+        while(i<n && t>0){
+            if(boxTypes[i][0]<=t){
+                t = t-boxTypes[i][0];
+                ans += (boxTypes[i][0] * boxTypes[i][1]);
+            }else{
+                ans += (t * boxTypes[i][1]);
+                t = 0;
             }
-            else if(size < boxTypes[i][0] && size>0){
-                maxunits  += size*boxTypes[i][1];
-                break;
-            }
+            i++;
         }
-        return maxunits;
+        return ans;
     }
 };
