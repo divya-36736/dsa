c9b4a34322ad10a3faa433b35c319644e4557a9d
divya-36736 <23bec036@nith.ac.in>
2026-06-27 09:15:36 +0530

    Time: 203 ms (33.04%), Space: 14 MB (29.69%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,45 +1,23 @@
 class Solution {
 public:
-    // int f(int i, int j, vector<int>&nums, vector<vector<int>>&dp){
-    //     if(i>j) return 0;
+    int solve(int i, int j, vector<int>&nums, vector<vector<int>>&dp){
+        if(i>j) return 0;
 
-    //     if(dp[i][j] != -1) return dp[i][j];
+        if(dp[i][j] != -1) return dp[i][j];
 
-    //     int maxi = INT_MIN;
-    //     for(int idx = i; idx<=j; idx++){
-    //         int cost = nums[i-1]*nums[idx]*nums[j+1];
-    //         int rem = f(i, idx-1, nums, dp) + f(idx+1, j, nums, dp);
-    //         maxi = max(maxi, cost+rem);
+        int maxi = -1e9;
+        for(int ind = i; ind<=j; ind++){
+            int cost = nums[i-1]*nums[ind]*nums[j+1]+solve(i, ind-1, nums, dp)+solve(ind+1, j, nums, dp);
 
-    //     }
-    //     return dp[i][j] = maxi;
-    // }
-
-
+            maxi = max(maxi, cost);
+        }
+        return dp[i][j] = maxi;
+    }
     int maxCoins(vector<int>& nums) {
         int n = nums.size();
         nums.push_back(1);
         nums.insert(nums.begin(), 1);
-        //memoization
-        //vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
-
-        //tabulation
-        //base case
-        vector<vector<int>>dp(n+2, vector<int>(n+2, 0));
-        for(int i = n; i>=1; i--){
-            for(int j = 1; j<=n; j++){
-                if(i>j) continue;
-
-                int maxi = INT_MIN;
-                for(int idx = i; idx<=j; idx++){
-                    int cost = nums[i-1] * nums[idx] * nums[j+1];
-                    int rem = dp[i][idx-1] + dp[idx+1][j];
-
-                    maxi = max(maxi, cost+rem);
-                }
-                dp[i][j] = maxi;   
-            }
-        }
-        return dp[1][n];
+        vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
+        return solve(1, n, nums, dp);
     }
 };
