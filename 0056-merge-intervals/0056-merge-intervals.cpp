04a93baa9de26006025b79f6c415b83b9d579c13
divya-36736 <23bec036@nith.ac.in>
2026-06-28 18:06:19 +0530

    Time: 5 ms (51.64%), Space: 23.9 MB (59.26%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -8,17 +8,11 @@ public:
         for(int i = 0; i<n; i++){
             int start = intervals[i][0];
             int end = intervals[i][1];
-            if(!ans.empty() && end <= ans.back()[1]){
-                continue;
+            if(ans.empty() || ans.back()[1] <  intervals[i][0]){
+                ans.push_back(intervals[i]);
+            }else{
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
             }
-            for(int j = i+1; j<n; j++){
-                if(intervals[j][0] <= end){
-                    end = max(intervals[j][1], end);
-                }else{
-                    break;
-                }
-            }
-            ans.push_back({start, end});
         }
         return ans;
     }
