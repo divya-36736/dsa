2712933a6f4cfa0698ebfaf988adc394ca22b2ea
divya-36736 <23bec036@nith.ac.in>
2026-06-28 23:12:46 +0530

    Time: 0 ms (100%), Space: 52.5 MB (26.62%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-        int maxIdx = 0;
+        int maxidx = 0;
         for(int i = 0; i<n; i++){
-            if(i>maxIdx) return false;
-            maxIdx = max(maxIdx, i+nums[i]);
-            if(maxIdx >= n-1) return true;
+            if(i > maxidx) return false;
+            maxidx = max(maxidx, i+nums[i]);
+
+            if(maxidx >= n-1) return true;
         }
-        
-        return false;
+        return true;
     }
 };
