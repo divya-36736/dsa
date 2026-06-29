59ff57ba5569836604be7f901b83a9dd5f6f9ed3
divya-36736 <23bec036@nith.ac.in>
2026-06-29 19:27:17 +0530

    Time: 0 ms (100%), Space: 20.6 MB (39.26%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         int n = nums.size();
-        int jumps = 0;
         int l = 0;
         int r = 0;
-        while(r<n-1){
-            int farpoint  = 0;
+        int jump= 0;
+        while(l<n-1 && r<n-1){
+            int farpoint = 0;
             for(int i = l; i<=r; i++){
                 farpoint = max(farpoint, i+nums[i]);
             }
-            l= r + 1;
+            l = r+1;
             r = farpoint;
-            jumps++;
+            jump++;
         }
-        return jumps;
+        return jump;
     }
 };
