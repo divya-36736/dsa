53d836d5882b8a7e22c7831c6d82a5129317b1b9
divya-36736 <23bec036@nith.ac.in>
2026-07-02 02:04:24 +0530

    Time: 0 ms (100%), Space: 8.8 MB (61.34%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,27 +1,40 @@
 class Solution {
 public:
-    void subs(int i, int n, int k, vector<vector<int>>& ans, vector<int>& ds, vector<int>& nums) {
-        if(ds.size() == k) {
-            if(n == 0) {
+
+    void solve(int n,
+               int i,
+               int k,
+               vector<vector<int>>& ans,
+               vector<int>& ds){
+
+        if(ds.size()==k){
+
+            if(n==0)
                 ans.push_back(ds);
-            }
+
             return;
         }
-        for(int j = i; j < nums.size(); j++) {
-            if(nums[j] > n) break;  // pruning
-            ds.push_back(nums[j]);
-            subs(j + 1, n - nums[j], k, ans, ds, nums);
+
+        for(int j=i;j<=9;j++){
+
+            if(j>n)
+                break;
+
+            ds.push_back(j);
+
+            solve(n-j,j+1,k,ans,ds);
+
             ds.pop_back();
         }
     }
 
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<int> nums;
-        for(int i = 1; i <= 9; i++) nums.push_back(i);
 
         vector<vector<int>> ans;
         vector<int> ds;
-        subs(0, n, k, ans, ds, nums);
+
+        solve(n,1,k,ans,ds);
+
         return ans;
     }
 };
