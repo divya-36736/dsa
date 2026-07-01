828fc0cb29f729f7dd2967798dd74f8b119afc98
divya-36736 <23bec036@nith.ac.in>
2026-07-02 03:03:49 +0530

    Time: 191 ms (82%), Space: 10.3 MB (93.11%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,46 +1,35 @@
 class Solution {
 public:
-    bool dfs(int i,int j, vector<vector<char>>& board, string word, int ind){
-        //base case
-        if(ind == word.size()){
-            return true;
-        }
+    bool dfs(int i, int j,int ind,  vector<vector<char>>&board, string &word){
+        if(ind == word.size()) return true;
 
         int n = board.size();
         int m = board[0].size();
+        if(i<0 || j<0 || i>=n || j>=m) return false;
 
-        if(i<0 || j<0 || i>=n || j>=m || board[i][j] != word[ind]) return false;
+        if(board[i][j] != word[ind]) return false;
 
         char temp = board[i][j]; //stored in temp
         board[i][j] = '#'; //mark it
-
-        int dx[] = {-1, 0, 1, 0};
+        
+        int dx[] = {1, 0, -1, 0};
         int dy[] = {0, -1, 0, 1};
-
         for(int dir = 0; dir<4; dir++){
-            int nx = i+ dx[dir];
+            int nx = i+dx[dir];
             int ny = j+dy[dir];
 
-            if(dfs(nx, ny, board, word, ind+1)){
-                board[i][j] = temp;
-                //unmark the #
-                return true;
-            }
-
-
+            if(dfs(nx, ny, ind+1, board, word)) return true;
         }
-        board[i][j] = temp ;//unmark
+        board[i][j] = temp;
         return false;
     }
-
     bool exist(vector<vector<char>>& board, string word) {
         int n = board.size();
         int m = board[0].size();
+        int x = word.size();
         for(int i = 0; i<n; i++){
             for(int j = 0; j<m; j++){
-                if(board[i][j] == word[0] && dfs(i,j, board, word, 0)){ //ind for traversing on word
-                    return true;
-                }
+                if(dfs(i, j,0, board, word)) return true;
             }
         }
         return false;
