a57e79c5c30561f341ebf2ad5bb5383bb9551ecf
divya-36736 <23bec036@nith.ac.in>
2026-07-02 00:14:04 +0530

    Time: 0 ms (100%), Space: 14 MB (83.97%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,42 +1,24 @@
 class Solution {
 public:
-    void solve(int i, int target, vector<int>& candidates,
-               set<vector<int>>& ans, vector<int>& res) {
-
-        if (target == 0) {
-            ans.insert(res);
-            return;
+    void subs(int i, int target, vector<vector<int>>&ans, vector<int>&ds, vector<int>&candidates){
+        if(target == 0){
+            ans.push_back(ds);
         }
-
-        int n = candidates.size();
-
-        for (int j = i; j < n; j++) {
-
-            // Skip duplicates
-            if (j > i && candidates[j] == candidates[j - 1])
-                continue;
-
-            // Since array is sorted
-            if (candidates[j] > target)
-                break;
-
-            res.push_back(candidates[j]);
-
-            solve(j + 1, target - candidates[j], candidates, ans, res);
-
-            res.pop_back();
+        for(int j = i; j<candidates.size(); j++){
+            if(j>i && candidates[j] == candidates[j-1]) continue; //if dublicate skip
+            if(candidates[j]>target) break; //if target se badi value hogi toh bresk kr jange
+            ds.push_back(candidates[j]);
+            subs(j+1, target-candidates[j], ans, ds, candidates); //every value use only once
+            ds.pop_back();
         }
+        return;
     }
-
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
 
         sort(candidates.begin(), candidates.end());
-
-        set<vector<int>> ans;
-        vector<int> res;
-
-        solve(0, target, candidates, ans, res);
-
-        return vector<vector<int>>(ans.begin(), ans.end());
+        vector<vector<int>>ans;
+        vector<int>ds;
+        subs(0, target, ans, ds, candidates);
+        return ans;
     }
 };
