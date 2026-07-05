01e624cfee7f5737bb5661b664d27fd719219540
divya-36736 <23bec036@nith.ac.in>
2026-07-05 16:22:03 +0530

    Time: 1912 ms (5.04%), Space: 9.3 MB (74.97%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,29 +1,39 @@
 class Solution {
 public:
-    bool isPalindrom(int i, int j, string &s){
-        while(i < j){
-            if(s[i] != s[j]) return false;
-            i++;
-            j--;
+    bool ispalindrom(string &s, int i, int j){
+        while(i<=j){
+            if(s[i++] != s[j--]) return false;
         }
         return true;
     }
 
+    int f(int i, string s, vector<int>&dp){
+        if(i == s.size()) return 0;
+        if(dp[i] != -1) return dp[i];
+
+        int mini = INT_MAX;
+        for(int j = i; j<s.size(); j++){
+            if(ispalindrom(s, i, j)){
+                int cost = 1+f(j+1, s, dp);
+                mini = min(mini, cost);
+            }
+        }
+        return dp[i] = mini;
+    }
     int minCut(string s) {
         int n = s.size();
-        vector<int> dp(n + 1, 0);
-
-        dp[n] = -1; 
-
-        for(int i = n - 1; i >= 0; i--){
-            int minCuts = INT_MAX;
-            for(int j = i; j < n; j++){
-                if(isPalindrom(i, j, s)){
-                    int cost = 1 + dp[j + 1];
-                    minCuts = min(minCuts, cost);
+        vector<int>dp(n+1, 0);
+        //return f(0, s, dp)-1;
+        dp[n] = -1;
+        for(int i = n-1; i>=0; i--){
+            int mini = INT_MAX;
+            for(int j = i; j<n; j++){
+                if(ispalindrom(s, i, j)){
+                    int cost = 1+dp[j+1];
+                    mini = min(mini, cost);
                 }
             }
-            dp[i] = minCuts;
+            dp[i] = mini;
         }
         return dp[0];
     }
