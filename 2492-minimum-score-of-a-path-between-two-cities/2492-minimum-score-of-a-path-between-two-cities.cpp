ad3b2177f2053472d13c7a2c1d944bd717ca194c
divya-36736 <23bec036@nith.ac.in>
2026-07-05 12:59:08 +0530

    Time: 56 ms (68.92%), Space: 134.1 MB (59.68%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,41 +1,39 @@
 class Solution {
 public:
     int minScore(int n, vector<vector<int>>& roads) {
-        vector<vector<pair<int,int>>> adj(n + 1);
+        vector<vector<pair<int, int>>>adj(n+1);
+        for(auto &it: roads){
+            int u = it[0];
+            int v = it[1];
+            int wt = it[2];
 
-        for(auto &road : roads){
-            int u = road[0];
-            int v = road[1];
-            int w = road[2];
-
-            adj[u].push_back({v, w});
-            adj[v].push_back({u, w});
+            adj[u].push_back({v, wt});
+            adj[v].push_back({u, wt});
         }
 
-        vector<bool> vis(n + 1, false);
-        queue<int> q;
-
+        queue<int>q;
+        vector<int>vis(n+1, 0);
         q.push(1);
-        vis[1] = true;
+        vis[1] = 1;
 
         int ans = INT_MAX;
 
         while(!q.empty()){
-
-            int node = q.front();
+            int it = q.front();
             q.pop();
 
-            for(auto &[nei, wt] : adj[node]){
+            for(auto &nbr: adj[it]){
+                int adjnode = nbr.first;
+                int adjwt = nbr.second;
 
-                ans = min(ans, wt);
+                ans = min(ans, adjwt);
 
-                if(!vis[nei]){
-                    vis[nei] = true;
-                    q.push(nei);
+                if(!vis[adjnode]){
+                    vis[adjnode] = 1;
+                    q.push(adjnode);
                 }
             }
         }
-
         return ans;
     }
 };
