fa45208906125671c339379029d2ba0359fafbc3
divya-36736 <23bec036@nith.ac.in>
2026-07-07 10:47:05 +0530

    Time: 0 ms (100%), Space: 8.9 MB (89.09%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -1,17 +1,17 @@
 class Solution {
-public:
-    long long sumAndMultiply(int n) {
-        long long sum = 0;
-        if(n == 0) return 0;
-        string ans = "";
-        string s = to_string(n);
-        for(int i = 0; i<s.size(); i++){
-            if(s[i] != '0'){
-                ans += s[i];
-                sum += s[i]-'0';
+    public:
+        long long sumAndMultiply(int n) {
+            long long x = 0;
+            int r = 1;
+            int sum_digits = 0;
+            while (n > 0) {
+                int d = n % 10;
+                n /= 10;
+                if (d == 0) continue;
+                x = d * r + x;
+                sum_digits += d;
+                r *= 10;
             }
+            return x * sum_digits;
         }
-        long long ans1 = stol(ans);
-        return ans1*sum;
-    }
 };
