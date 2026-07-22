9b8fa0d354c9de0f4658f264f6caf65b932df717
divya-36736 <23bec036@nith.ac.in>
2026-07-22 23:41:48 +0530

    Time: 0 ms (100%), Space: 18.5 MB (66.36%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -11,14 +11,15 @@
  */
 class Solution {
 public:
-    bool ismirror(TreeNode*p, TreeNode*q){
-        if(!p && !q) return true;
-        if((p && !q) || (!p && q)) return false;
+    bool isSameSymmetric(TreeNode* root1, TreeNode* root2){
+        if(root1 == NULL || root2 == NULL){
+            return root1 == root2;
+        }
 
-        return (p->val == q->val) && ismirror(p->left, q->right) && ismirror(p->right, q->left);
+        return (root1->val == root2->val) && isSameSymmetric(root1->left, root2->right) && isSameSymmetric(root1->right, root2->left);
     }
     bool isSymmetric(TreeNode* root) {
-        if(!root) return true;
-        return ismirror(root->left, root->right);
+       if(!root) return true;
+       return isSameSymmetric(root->left, root->right); 
     }
 };
