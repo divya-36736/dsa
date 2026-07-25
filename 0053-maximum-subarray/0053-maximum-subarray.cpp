35641de2cab0d12db39b031dba71695f8f40591b
divya-36736 <23bec036@nith.ac.in>
2026-07-25 09:00:39 +0530

    Time: 0 ms (100%), Space: 71.8 MB (53.04%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -2,52 +2,18 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int n = nums.size();
-        //int maxi = INT_MIN;
-
-        // Brute solution
-        //  for(int i = 0; i<n; i++){
-        //      for(int j = i; j<n; j++){
-        //          int sum = 0;
-        //          for(int k = i; k<=j; k++){
-        //              sum += nums[k];
-        //          }
-        //          maxi = max(maxi, sum);
-        //      }
-        //  }
-        //  return maxi;
-
-        // Better solution
-        //  for(int i = 0; i < n; i++) {
-        //      int sum = 0;
-        //      for(int j = i; j < n; j++) {
-        //          sum += nums[j];
-        //          maxi = max(maxi, sum);
-        //      }
-        //  }
-        //  return maxi;
-
-        long long maxi = LONG_MIN; // maximum sum
-        long long sum = 0;
-
-        for (int i = 0; i < n; i++) {
-
+        int maxi = INT_MIN;
+        int sum = 0;
+        for(int i = 0; i<n; i++){
             sum += nums[i];
 
-            if (sum > maxi) {
-                maxi = sum;
-            }
+            maxi = max(maxi, sum);
 
-            // If sum < 0: discard the sum calculated
-            if (sum < 0) {
-                sum = 0;
+            if(sum<0){
+                sum = 0 ;
             }
         }
-
-        // To consider the sum of the empty subarray
-        // uncomment the following check:
-
-        // if (maxi < 0) maxi = 0;
-
         return maxi;
+
     }
 };
