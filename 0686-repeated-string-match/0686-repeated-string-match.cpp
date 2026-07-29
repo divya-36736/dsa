6526289798208db1b75d8c93fceca3861f2b4650
divya-36736 <23bec036@nith.ac.in>
2026-07-29 22:52:34 +0530

    Time: 7 ms (42.97%), Space: 9.6 MB (93.42%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.cpp
+++ 0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -1,34 +1,22 @@
 class Solution {
 public:
-    int isSubstr(string a1, string b){
-        int n1 = a1.size();
-        int n2 = b.size();
-        for(int i = 0; i<=(n1-n2); i++){
-            int j;
-            for(j = 0; j<n2; j++){
-                if(a1[i+j] != b[j]){
-                    break;
-                }
-            }
-            if(j == n2) return true;
-        }
-        return false;
-    }
     int repeatedStringMatch(string a, string b) {
-        string a1 = a;
-        int cnt = 1;
-        while(a1.size() < b.size()){
-            a1 += a;
-            cnt++;
+        string temp = a;
+        int count = 1;
+
+        while (temp.size() < b.size()) {
+            temp += a;
+            count++;
         }
-        if(isSubstr(a1, b)){
-            return cnt;
-        }
-        a1 += a;
-        cnt++;
-        if(isSubstr(a1, b)){
-            return cnt;
-        }
+
+        if (temp.find(b) != string::npos)
+            return count;
+
+        temp += a;
+
+        if (temp.find(b) != string::npos)
+            return count + 1;
+
         return -1;
     }
 };
