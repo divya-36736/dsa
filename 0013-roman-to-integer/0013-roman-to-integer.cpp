46c081b6c15daff0ea65964769cdda74b27cc13f
divya-36736 <23bec036@nith.ac.in>
2026-07-29 17:37:05 +0530

    Time: 0 ms (100%), Space: 9.9 MB (94.67%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     int convert(char c){
-        
         if(c == 'I') return 1;
-        else if(c == 'X') return 10;
         else if(c == 'V') return 5;
+        else if(c == 'X') return 10;
         else if(c == 'L') return 50;
         else if(c == 'C') return 100;
         else if(c == 'D') return 500;
-        else return 1000;
+        return 1000;
     };
     int romanToInt(string s) {
-        int sum = 0;
         int n = s.size();
+        int sum = 0;
         for(int i = 0; i<n-1; i++){
             if(convert(s[i]) < convert(s[i+1])){
-                //-hoga
                 sum -= convert(s[i]);
-            }
-            else{
+            }else{
                 sum += convert(s[i]);
             }
         }
