ae3e82f88564eab61bc733b9a253239032c9f4bb
divya-36736 <23bec036@nith.ac.in>
2026-08-01 18:32:27 +0530

    Time: 0 ms (100%), Space: 9.3 MB (25.67%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,20 +1,67 @@
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
+    long long mod = 1e9 + 7;
 
+    long long hashValue(string s, long long base, int m) {
+        long long factor = 1;
+        long long ans = 0;
+
+        for (int i = m - 1; i >= 0; i--) {
+            ans = (ans + ((s[i] - 'a' + 1) * factor) % mod) % mod;
+            factor = (factor * base) % mod;
+        }
+
+        return ans;
+    }
+
+    int strStr(string haystack, string needle) {
         int n = haystack.size();
         int m = needle.size();
 
-        for(int i=0;i<=n-m;i++){
+        if (n < m)
+            return -1;
 
-            int j=0;
+        long long base = 26;
+        long long max_wt = 1;
 
-            while(j<m && haystack[i+j]==needle[j]){
-                j++;
+        // base^(m-1)
+        for (int i = 1; i < m; i++)
+            max_wt = (max_wt * base) % mod;
+
+        long long hashneedle = hashValue(needle, base, m);
+        long long hashhay = 0;
+
+        for (int i = 0; i <= n - m; i++) {
+
+            if (i == 0) {
+                hashhay = hashValue(haystack, base, m);
+            } else {
+
+                hashhay =
+                    (((hashhay -
+                       ((haystack[i - 1] - 'a' + 1) * max_wt) % mod +
+                       mod) %
+                          mod) *
+                         base +
+                     (haystack[i + m - 1] - 'a' + 1)) %
+                    mod;
             }
 
-            if(j==m)
-                return i;
+            if (hashneedle == hashhay) {
+
+                bool match = true;
+
+                for (int j = 0; j < m; j++) {
+
+                    if (needle[j] != haystack[i + j]) {
+                        match = false;
+                        break;
+                    }
+                }
+
+                if (match)
+                    return i;
+            }
         }
 
         return -1;
