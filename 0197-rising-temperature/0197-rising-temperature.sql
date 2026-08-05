d18867854439f31970061656abf090f4b9bc7647
divya-36736 <23bec036@nith.ac.in>
2026-08-06 00:19:57 +0530

    Time: 1285 ms (5%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,11 +1,5 @@
 # Write your MySQL query statement below
-#W1 current and w2 is perivous
-#brute force
-SELECT id
-FROM Weather w
-WHERE temperature >
-(
-    SELECT temperature
-    FROM Weather
-    WHERE recordDate = DATE_SUB(w.recordDate, INTERVAL 1 DAY)
-);
+select w1.id from Weather w1 
+inner join Weather w2
+on DATEDIFF(w1.recordDate, w2.recordDate) = 1
+and w1.temperature > w2.temperature
