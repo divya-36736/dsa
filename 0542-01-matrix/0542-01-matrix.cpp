8e24100ff1259838bb777ced1146dae9d8fdd16c
divya-36736 <23bec036@nith.ac.in>
2026-08-07 17:46:38 +0530

    Time: 30 ms (19.93%), Space: 38.8 MB (38.12%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -2,48 +2,40 @@ class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n = mat.size();
-        int m = mat[0].size();
-
-        vector<vector<int>>vis(n, vector<int>(m ,0)); //visited mats
-        vector<vector<int>>dist(n, vector<int>(m , 0)); //distance matrix
-
-        //BFS traversal
-        queue<pair<pair<int, int>,int>>q; //for(x,y,(dist))
-
+        int m =mat[0].size();
+        queue<pair<pair<int, int>,int>>q;
+        vector<vector<int>>vis(n, vector<int>(m, 0));
+        vector<vector<int>>dist(n, vector<int>(m, 0));
         for(int i = 0; i<n; i++){
             for(int j = 0; j<m; j++){
-                if(mat[i][j] == 0){  //ydi mat[i][j] == 0, toh 0 ki 0 sedistance 0 hi hogi
-                    q.push({{i,j},0}); //push them in queue
-                    vis[i][j] = 1;  ///visted
-                }
-                else{
-                    vis[i][j] = 0; //ydi 0 nhi hai toh unhe unvisited hi rakhenge
+                if(mat[i][j] == 0){
+                    q.push({{i,j}, 0});
+                    vis[i][j] = 1;
                 }
             }
         }
 
-        int delrow[] = {-1, 0, 1, 0};
-        int delcol[]={0, -1, 0, 1};
+        int dx[] = {1, 0, -1, 0};
+        int dy[] = {0, -1, 0, 1};
+
         while(!q.empty()){
-            int x = q.front().first.first;
-            int y = q.front().first.second;
-            int step = q.front().second;
+            auto it = q.front();
             q.pop();
-            dist[x][y] = step;
 
-            //for check the up down left right
-            for(int i = 0; i<4; i++){
-                int nx  = x + delrow[i];
-                int ny = y + delcol[i];
+            int x = it.first.first;
+            int y = it.first.second;
+            int step = it.second;
 
-                if(nx<0 || ny<0|| nx>=n|| ny>=m ||vis[nx][ny]) continue;
-
-                vis[nx][ny] = 1;
-                q.push({{nx,ny}, step+1});
-
+            for(int dir = 0; dir<4; dir++){
+                int nx = x+dx[dir];
+                int ny = y+dy[dir];
+                if(nx>=0 && nx<n && ny>=0 && ny<m && !vis[nx][ny]){
+                    vis[nx][ny] = 1;
+                    dist[nx][ny] = step+1;
+                    q.push({{nx, ny}, step+1});
+                }
             }
         }
-
         return dist;
     }
 };
