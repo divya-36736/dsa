8e2ad6cbc51215d1ed752fbf40257b0e513dde14
divya-36736 <23bec036@nith.ac.in>
2026-08-23 12:50:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -3,13 +3,18 @@ public:
     int maxProduct(vector<int>& nums) {
         int n = nums.size();
         int ans = INT_MIN;
+        int prod = 1;
         for(int i = 0; i<n; i++){
-           int maxpr = 1;
-            for(int j = i; j<n; j++){
-                maxpr *= nums[j];
-                ans = max(ans, maxpr);
-            }
+            prod *= nums[i];
+            ans = max(ans, prod);
         }
+
+        prod = 1;
+        for(int i = n-1; i>=0; i--){
+            prod *= nums[i];
+            ans = max(ans, prod);
+        }
+        
         return ans;
     }
 };
