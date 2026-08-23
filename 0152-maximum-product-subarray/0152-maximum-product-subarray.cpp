1f9e3b3b5147764563de2f238028aaecf6bd46d5
divya-36736 <23bec036@nith.ac.in>
2026-08-23 12:51:37 +0530

    Time: 0 ms (100%), Space: 17.8 MB (60.77%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -7,12 +7,14 @@ public:
         for(int i = 0; i<n; i++){
             prod *= nums[i];
             ans = max(ans, prod);
+            if(prod == 0) prod = 1;
         }
 
         prod = 1;
         for(int i = n-1; i>=0; i--){
             prod *= nums[i];
             ans = max(ans, prod);
+            if(prod == 0) prod = 1;
         }
         
         return ans;
