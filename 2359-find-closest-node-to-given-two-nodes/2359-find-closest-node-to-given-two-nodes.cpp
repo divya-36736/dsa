29f13a3622122514e3dd53cae5e76c2f40efda65
divya-36736 <23bec036@nith.ac.in>
2026-08-24 10:22:44 +0530

    Time: 24 ms (37.95%), Space: 112.3 MB (30.33%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
-    void dfs(vector<int>&edges, int node, vector<int>&dist, vector<int>&vis){
+    void bfs(vector<int>&edges, int node, vector<int>&dist){
+        int n = edges.size();
+        
+        queue<int>q;
+        dist[node] = 0;
+        q.push(node);
+        vector<int>vis(n, 0);
         vis[node] = 1;
 
-        int v = edges[node];
-        if(v != -1 && !vis[v]){
-            vis[v] = 1;
-            dist[v] = 1+dist[node];
-            dfs(edges, v, dist, vis);
+        while(!q.empty()){
+            int it = q.front();
+            q.pop();
+
+            int v = edges[it];
+            if(v != -1 && !vis[v]){
+                vis[v] = 1;
+                dist[v] = 1+dist[it];
+                q.push(v);
+            }
         }
+
     }
     int closestMeetingNode(vector<int>& edges, int node1, int node2) {
         int n = edges.size();
+
         vector<int>dist1(n, INT_MAX);
         vector<int>dist2(n, INT_MAX);
 
-        vector<int>vis1(n, 0);
-        vector<int>vis2(n, 0);
-
-        dist1[node1] = 0;
-        dist2[node2] = 0;
-
-        dfs(edges, node1, dist1, vis1);
-        dfs(edges, node2, dist2, vis2);
+        bfs(edges, node1, dist1);
+        bfs(edges, node2, dist2);
 
         int minNode = -1;
         int minDis = INT_MAX;
