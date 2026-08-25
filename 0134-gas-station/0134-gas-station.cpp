cf1de74e2de7ff9838576d242bc6e806c70303c7
divya-36736 <23bec036@nith.ac.in>
2026-08-25 09:26:46 +0530

    Time: 0 ms (100%), Space: 112.5 MB (26.12%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -1,50 +1,26 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int g = gas.size();
-        int c = cost.size();
-        //total gas
+        int n = gas.size();
+        int m = cost.size();
         int totgas = 0;
         int totcost = 0;
-        // for(int val:gas){
-        //     totgas += val;
-        // }
-        // //total cost
-        // for(int val: cost){
-        //     totcost += val;
-        // }
-        // //if this fail
-        // if(totgas < totcost){
-        //     return -1;
-        // } 
-        //in above 2 different loops use krene ki bjay 
-        //hum sara ek hi loop me kr dakte hai 
-
-        int st = 0;
-        int currgas = 0;
-        for(int i = 0; i<g; i++){
-            totgas+=gas[i];
-            totcost += cost[i];
-            currgas += (gas[i]-cost[i]);
-            if(currgas<0){
-                st = i+1;
-                currgas = 0;
-            }
+       for(int i = 0; i<n; i++){
+        totgas += gas[i];
+       }
+       for(int i = 0; i<n; i++){
+        totcost += cost[i];
+       }
+       if(totgas < totcost) return -1;
+       int currgas = 0;
+       int st = 0;
+       for(int i = 0; i<n; i++){
+        currgas += gas[i] - cost[i];
+        if(currgas < 0){
+            st = i+1;
+            currgas = 0;
         }
-
-        return totgas < totcost? -1:st;
-
-
-        // int st = 0;
-        // int currgas = 0;
-        // for(int i = 0; i<g; i++){
-        //     currgas += (gas[i]-cost[i]);
-        //     if(currgas<0){
-        //         st = i+1;
-        //         currgas = 0;
-        //     }
-        // }
-
-        // return st;
+       }
+       return st;
     }
 };
