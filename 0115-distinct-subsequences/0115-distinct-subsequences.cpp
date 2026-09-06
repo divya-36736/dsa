f2698272562be406a369aa576d9450687fc43245
divya-36736 <23bec036@nith.ac.in>
2026-09-06 09:17:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,35 +1,26 @@
 class Solution {
 public:
-    int recur(int i, int j, string &s, string &t, vector<vector<int>>&dp){
-        if(j<0) return 1;
-        if(i<0) return 0;
+    int f(int i, int j, string s, string t, vector<vector<int>>&dp){
+        if(j == t.size()) return 1;
+        if(i == s.size()) return 0;
+
         if(dp[i][j] != -1) return dp[i][j];
+
         if(s[i] == t[j]){
-            dp[i][j] = recur(i-1, j-1, s, t, dp)+recur(i-1, j, s, t, dp);
+            //take
+            int take = f(i+1, j+1, s, t, dp);
+            int nottake = f(i+1, j, s, t, dp);
+            dp[i][j] = take+nottake;
         }
         else{
-            dp[i][j] = recur(i-1, j, s, t, dp);
+            dp[i][j] =  f(i+1, j, s, t, dp);
         }
         return dp[i][j];
     }
     int numDistinct(string s, string t) {
         int n = s.size();
         int m = t.size();
-        //vector<vector<double>>dp(n+1, vector<double>(m+1, 0));
-        //return recur(n-1, m-1, s, t, dp);
-        vector<double>prev(m+1, 0);
-        
-    
-        prev[0] = 1;
-        
-         
-        for(int i = 1; i<=n; i++){
-            for(int j = m; j>=1; j--){
-                if(s[i-1] == t[j-1]){
-                    prev[j] = prev[j-1] + prev[j];
-                }
-            }
-        }
-        return (int)prev[m];
+        vector<vector<int>>dp(n, vector<int>(m, -1));
+        return f(0, 0, s, t, dp);
     }
 };
