860dc3ca71548b1c5bcb78fa1116591f8ad8c7ee
divya-36736 <23bec036@nith.ac.in>
2026-09-07 23:47:11 +0530

    Time: 3 ms (34.25%), Space: 77.1 MB (45.85%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,37 +11,27 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        // Dummy node banaya, jisse result list ka head store ho sake
         ListNode* dummy = new ListNode();
-        ListNode* temp = dummy; // temp pointer list banane ke liye
-        int carry = 0;          // carry ko store karega
+        ListNode* temp = dummy;
+        int carry = 0;
 
-        // Jab tak dono list me nodes bachi ho ya carry bacha ho
-        while (l1 != NULL || l2 != NULL || carry) {
-            int sum = 0; // current digits ka sum store karega
-
-            // Agar l1 ka current node hai, to uska value sum me add karo
-            if (l1 != NULL) {
+        while(l1 != NULL || l2 != NULL || carry){
+            int sum = 0;
+            if(l1 != NULL){
                 sum += l1->val;
-                l1 = l1->next; // l1 ko aage badhao
+                l1 = l1->next;
             }
-
-            // Agar l2 ka current node hai, to uska value sum me add karo
-            if (l2 != NULL) {
+            if(l2!= NULL){
                 sum += l2->val;
-                l2 = l2->next; // l2 ko aage badhao
+                l2 = l2->next;
             }
+            sum += carry;
+            carry = sum/10;
 
-            sum += carry;     // pichle step ka carry add karo
-            carry = sum / 10; // naya carry calculate karo
-
-            // sum ka last digit nayi list me node banake add karo
-            ListNode* node = new ListNode(sum % 10);
+            ListNode* node = new ListNode(sum%10);
             temp->next = node;
-            temp = temp->next; // temp pointer ko aage badhao
+            temp = temp->next;
         }
-
-        // dummy ka next hi actual result ka head hoga
         return dummy->next;
     }
 };
