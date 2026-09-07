9f0ffe6e138a7a649c016bbdd073b3839e614e8f
divya-36736 <23bec036@nith.ac.in>
2026-09-08 02:02:51 +0530

    Time: 4 ms (18.65%), Space: 95.8 MB (25.16%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -4,6 +4,7 @@ public:
         int n1 = nums1.size();
         int n2 = nums2.size();
         vector<int>ans;
+
         int i = 0;
         int j = 0;
         while(i<n1 && j<n2){
@@ -16,16 +17,14 @@ public:
             }
         }
 
-        // add remaining elements
-        while(i < n1) ans.push_back(nums1[i++]);
-        while(j < n2) ans.push_back(nums2[j++]);
+        while(i<n1) ans.push_back(nums1[i++]);
+        while(j<n2) ans.push_back(nums2[j++]);
 
         int n = ans.size();
-
-        if(n % 2 == 1){
+        if(n%2 == 1){
             return ans[n/2];
-        } else {
-            return (ans[n/2 - 1] + ans[n/2]) / 2.0;
+        }else{
+            return (ans[n/2-1]+ans[n/2])/2.0;
         }
     }
 };
