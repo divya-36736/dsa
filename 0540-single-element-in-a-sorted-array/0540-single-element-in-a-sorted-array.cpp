0e251ecd20a14ff1928f36c212a65b2f53b3ef0e
divya-36736 <23bec036@nith.ac.in>
2026-09-08 23:55:31 +0530

    Time: 0 ms (100%), Space: 26.2 MB (70.91%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -2,28 +2,18 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         int n = nums.size();
+        int st = 0;
+        int end = n-1;
+        while(st<end){
+            int mid = st + (end-st)/2;
 
-        if (n == 1) return nums[0];
-        if (nums[0] != nums[1]) return nums[0];
-        if (nums[n-1] != nums[n-2]) return nums[n-1];
-
-        int st = 1;
-        int end = n - 2;
-
-        while (st <= end) {
-            int mid = st + (end - st) / 2;
-
-            if (nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1])
-                return nums[mid];
-
-            if ((mid % 2 == 1 && nums[mid] == nums[mid-1]) ||
-                (mid % 2 == 0 && nums[mid] == nums[mid+1])) {
-                st = mid + 1;
-            } else {
-                end = mid - 1;
+            if(mid%2 == 1) mid--; //bcz before single number pair is e->o after single number pair is o->e
+            if(nums[mid] == nums[mid+1]){
+                st = mid+2;
+            }else{
+                end = mid;
             }
         }
-
-        return -1;
+        return nums[st];
     }
 };
