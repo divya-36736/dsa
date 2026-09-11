d19f63c4ba74331ea5c941590124bfd20fc8e36d
divya-36736 <23bec036@nith.ac.in>
2026-09-11 06:17:09 +0530

    Time: 6 ms (27.82%), Space: 17.1 MB (33.05%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -2,20 +2,19 @@ class Solution {
 public:
     string largestNumber(vector<int>& nums) {
         int n = nums.size();
-        //convert to string
         vector<string>arr;
-        for(int n:nums) arr.push_back(to_string(n));
-        //sort now with comparision
-        sort(arr.begin(), arr.end(), [](string &a, string &b){
-            return a+b > b+a; //comparison according to this 
-        });
-        if(arr[0] == "0") return "0";
 
-        //add all strings
-        string result = "";
-        for(string &r : arr){
-            result += r;
+        for(int num:nums) arr.push_back(to_string(num));
+
+        sort(arr.begin(), arr.end(), [](string &a, string &b){
+            return a+b>b+a;
+        });
+
+        string res;
+        for(string s: arr){
+            res += s;
         }
-        return result;
+        if(res[0] == '0') return "0";
+        return res;
     }
 };
