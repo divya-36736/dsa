9cddf42f24d470742a503914706b52a57d98f04b
divya-36736 <23bec036@nith.ac.in>
2026-09-11 10:18:30 +0530

    Time: 0 ms (100%), Space: 9.5 MB (5.32%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,35 +1,21 @@
 class Solution {
 public:
-    int count(int i, int j, int m, int n, vector<vector<int>>&dp){
-        if(i == 0 && j == 0) return 1;
+    int help(int i, int j, vector<vector<int>>&dp){
+        if(i == 0 && j == 0){
+            return 1;
+        }
+
         if(i<0 || j<0) return 0;
 
         if(dp[i][j] != -1) return dp[i][j];
 
-        int up = count(i-1, j, m, n, dp);
-        int left = count(i, j-1, m, n, dp);
+        int up = help(i-1, j, dp);
+        int left = help(i, j-1, dp);
+
         return dp[i][j] = up+left;
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m, vector<int>(n, 0));
-        //return count(m-1, n-1, m, n, dp);
-        vector<int>prev(n, 0);
-        prev[0] = 1;
-
-        for(int i = 0; i<m; i++){
-            vector<int>temp(n, 0);
-            for(int j = 0; j<n; j++){
-                if(i == 0 && j == 0){
-                    temp[j] = 1;
-                    continue;
-                }
-                int up = 0, left = 0;
-                if(i>0) up = prev[j];
-                if(j>0) left = temp[j-1];
-                temp[j] = up+left;
-            }
-            prev = temp;
-        }
-        return prev[n-1];
+        vector<vector<int>>dp(m, vector<int>(n,-1));
+        return help(m-1, n-1, dp);
     }
 };
