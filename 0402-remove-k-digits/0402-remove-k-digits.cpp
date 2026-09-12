0609349a9695f008b22ae8d8a26e06c0bb98a323
divya-36736 <23bec036@nith.ac.in>
2026-09-12 14:43:22 +0530

    Time: 1 ms (76.53%), Space: 12 MB (17.47%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        int n = num.size();
-        stack<char>st;
-        
-        for(int i = 0; i<n; i++){
-            while(k>0 && !st.empty() && st.top() > num[i]){
-                k--;
+        stack<char> st;
+
+        for(char c : num) {
+            while(!st.empty() && k > 0 && st.top() > c) {
                 st.pop();
+                k--;
             }
-            st.push(num[i]);
+
+            st.push(c);
         }
-        //edge cases
-        //stil k
-        while(!st.empty() && k>0) {
+
+        while(k > 0 && !st.empty()) {
             st.pop();
             k--;
         }
 
-        if(st.empty()) return "0";
+        string ans;
 
-        string res = "";
-        while(!st.empty()){
-            res += st.top();
+        while(!st.empty()) {
+            ans += st.top();
             st.pop();
         }
-        //trim front zeros
-        while(res.size() != 0 && res.back() == '0'){
-            res.pop_back();
+
+        reverse(ans.begin(), ans.end());
+
+        int i = 0;
+        while(i < ans.size() && ans[i] == '0') {
+            i++;
         }
-        reverse(res.begin(), res.end());
-        return res.empty() ? "0" : res;
+
+        ans = ans.substr(i);
+
+        return ans.empty() ? "0" : ans;
     }
 };
