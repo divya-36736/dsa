6d744965f86bfdeaec25f6cda099e6bf2f009820
divya-36736 <23bec036@nith.ac.in>
2026-09-13 09:49:01 +0530

    Time: 8 ms (60.32%), Space: 71.6 MB (46.99%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -2,10 +2,12 @@ class Solution {
 public:
     int atmost(vector<int>&nums, int k){
         int n = nums.size();
+        
         int l = 0;
         int r = 0;
-        int sum = 0;
         int cnt = 0;
+        long long sum  = 0;
+
         while(r<n){
             sum += (nums[r]%2);
             while(sum > k){
@@ -18,11 +20,6 @@ public:
         return cnt;
     }
     int numberOfSubarrays(vector<int>& nums, int k) {
-        //this question is also consider the same as binarysubarray 
-        //bcz we need the odd numbers theb we can put the odd with 1 and
-        //even with the 0 and we require the k numbers of odd numbers then we 
-        //can say same as gaol is k
-        int n = nums.size();
-        return atmost(nums, k)-atmost(nums, k-1);
+        return atmost(nums, k) - atmost(nums, k-1);
     }
 };
