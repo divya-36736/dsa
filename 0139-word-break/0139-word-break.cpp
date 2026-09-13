3f49562152779c9b1271513eaad5ed703177b314
divya-36736 <23bec036@nith.ac.in>
2026-09-13 11:08:14 +0530

    Time: 0 ms (100%), Space: 13.3 MB (60.74%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
-    bool backtrack(int i, string& s, unordered_set<string>& dict, vector<int>&dp) {
-//Start from index 0 of string s. At each step,try every word in wordDict.
-//If a word matches the substring starting at the current index :
-//Recursively call backtrack with the new index =start + word.size().
-//If you reach start ==s.size(), return true.
+    bool f(int i, string s, unordered_set<string>&Dict, vector<int>&dp){
+        if(i == s.size()) return true;
 
-//base case
-            if (i == s.size()) return true; // reached the end successfully
-            if(dp[i] != -1) return dp[i];
-        for (string word : dict) {
+        if(dp[i] != -1) return dp[i];
+
+        for(auto &word:Dict){
             int len = word.size();
-            if (i + len <= s.size() && s.substr(i, len) == word) {
-                if (backtrack(i + len, s, dict, dp))
-                    return dp[i] = true;
+            if(i+len <= s.size() && s.substr(i, len) == word){
+                if(f(i+len, s, Dict, dp)) return dp[i] = true;
             }
         }
         return dp[i] = false;
     }
-
     bool wordBreak(string s, vector<string>& wordDict) {
-        vector<int>dp(s.size(), -1);
-        unordered_set<string> dict(wordDict.begin(),
-                                   wordDict.end()); // remove repetation
-        return backtrack(0, s, dict, dp);               // index , string s, set
+        int n = s.size();
+        unordered_set<string>word(wordDict.begin(), wordDict.end());
+        vector<int>dp(n, -1);
+        return f(0, s, word, dp);
     }
 };
