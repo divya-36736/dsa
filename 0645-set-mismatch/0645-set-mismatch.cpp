0982ca357ed9e46ffc0de918ad27c838dcddb7e7
divya-36736 <23bec036@nith.ac.in>
2026-09-13 09:06:51 +0530

    Time: 0 ms (100%), Space: 26.3 MB (37.83%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -2,19 +2,16 @@ class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int,int> freq;
-
-        // Count occurrences of each number
-        for(int num : nums) {
+        vector<int>freq(n+1, 0);
+        for(int num: nums){
             freq[num]++;
         }
-
-        int dup = -1, missing = -1;
-        for(int i = 1; i <= n; i++) {
+        int dup = -1;
+        int miss = -1;
+        for(int i = 1; i<=n; i++){
             if(freq[i] == 2) dup = i;
-            if(freq[i] == 0) missing = i;
+            if(freq[i] == 0) miss = i;
         }
-
-        return {dup, missing};
+        return {dup, miss};
     }
 };
